40e31153f652e3490fa451e7523c20926a17eb76
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-11-01 10:42:04 +0000

    Add bower resolution configuration to webpack js config plugin

--- packages/webpack-js-config/src/configuration.ts
+++ packages/webpack-js-config/src/configuration.ts
@@ -1,3 +1,5 @@
+import BowerResolvePlugin from 'bower-resolve-webpack-plugin'
+
 export interface Options {
   /** You can test your browserslist string at https://browserl.ist/ */
   browserslist?: string
@@ -18,6 +20,27 @@ export function configuration(options: Options) {
   const opts = { ...defaults, ...options }
 
   return {
+    resolve: {
+      plugins: [
+        // Handle a Bower manifest specifying `main` as an array
+        new BowerResolvePlugin()
+      ],
+
+      // In which folders the resolver look for modules relative paths are
+      // looked up in every parent folder (like node_modules) absolute
+      // paths are looked up directly the order is respected
+      modules: ['bower_components', 'node_modules'],
+
+      // package description files
+      descriptionFiles: ['bower.json', 'package.json'],
+
+      // package.json / bower.json
+      // fields for package resolution
+      mainFields: ['browser', 'module', 'main'],
+
+      // file names for directory resolution
+      mainFiles: ['index', 'main']
+    },
     module: {
       rules: [
         {
