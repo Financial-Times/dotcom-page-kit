eb380fbca6c44f6b01696814ba2193229e9025d0
Matt Hinchliffe <matt@maketea.co.uk>
2018-11-07 13:54:43 +0000

    Remove sample package

--- packages/sample-package/src/__test__/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import subject from '..'
-
-describe('sample-package', () => {
-  it('multiplies the given number by 5', () => {
-    expect(subject(1)).toEqual(5)
-    expect(subject(2)).toEqual(10)
-    expect(subject(3)).toEqual(15)
-  })
-})
--- packages/sample-package/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { enumerator } from './enumerator'
-
-export default (number: number) => {
-  if (typeof number !== 'number') {
-    throw TypeError('NO DO NOT DO THAT')
-  }
-
-  return number * enumerator
-}
