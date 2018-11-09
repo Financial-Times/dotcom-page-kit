a2bd13942fb27264fb0579d6537d45237ea3f902
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2018-11-09 12:07:07 +0000

    Tidy up references to core-ui

--- packages/anvil-plugin-babel/babel.js
+++ packages/anvil-plugin-babel/babel.js
@@ -2,10 +2,11 @@
  * This file is meant to export the babel preset so that it can be used in a babelrc as follows:
  * {
  *    "preset": [
- *      "coreui-plugin-babel/babel"
+ *      "@financial-times/anvil-plugin-babel/babel"
  *    ]
  * }
  *
  */
 
-module.exports = require('./dist/cjs/babel')
+// NOTE: `./dist/cjs/babel` will be present in the distributed package, so it's absence should not cause lint checks to fail
+module.exports = require('./dist/cjs/babel') // eslint-disable-line import/no-unresolved
--- packages/anvil-plugin-esnext/babel.js
+++ packages/anvil-plugin-esnext/babel.js
@@ -2,10 +2,11 @@
  * This file is meant to export the babel preset so that it can be used in a .babelrc file as follows:
  * {
  *    "preset": [
- *      "coreui-plugin-js-next/babel"
+ *      "@financial-times/anvil-plugin-js-next/babel"
  *    ]
  * }
  *
  */
 
-module.exports = require('./dist/cjs/babel')
+// NOTE: `./dist/cjs/babel` will be present in the distributed package, so it's absence should not cause lint checks to fail
+module.exports = require('./dist/cjs/babel') // eslint-disable-line import/no-unresolved
