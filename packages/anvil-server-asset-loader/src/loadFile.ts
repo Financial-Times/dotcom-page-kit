64deb3d3af3c8f4b1197c1ac5a111f5adc9d3ed0
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-11-13 12:29:11 +0000

    Disable file contents caching by default

--- packages/anvil-server-asset-loader/src/loadFile.ts
+++ packages/anvil-server-asset-loader/src/loadFile.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 // hold the file contents in memory for the lifecycle of the app
 const store = new Map()
 
-export function loadFile(fullPath: string, cache: boolean = true): string {
+export function loadFile(fullPath: string, cache: boolean = false): string {
   if (store.has(fullPath)) {
     return store.get(fullPath)
   } else {
