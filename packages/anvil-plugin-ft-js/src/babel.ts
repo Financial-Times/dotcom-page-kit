a8f2242c70b71825bc514f48341559db4c95542f
Matt Hinchliffe <matt.hinchliffe@ft.com>
2018-11-21 14:30:24 +0000

    Refactor webpack ft js plugin to use config provided by context and add code comments

--- packages/anvil-plugin-ft-js/src/babel.ts
+++ packages/anvil-plugin-ft-js/src/babel.ts
@@ -1,4 +1,4 @@
-import { Settings } from './types'
+import { PluginSettings } from './types'
 import { CliContext } from '@financial-times/anvil'
 
 /**
@@ -8,17 +8,18 @@ import { CliContext } from '@financial-times/anvil'
  * When used in such a manner, there will be no args supplied to the function,
  * hence why the `context` arg is optional.
  */
-export default (context?: CliContext, settings: Settings = {}) => {
+export default (context?: CliContext) => {
+  const settings: PluginSettings = context ? context.config.settings['ft-js'] : {}
+
   const jsx = {
     pragma: settings.jsxPragma || 'h',
     fragment: settings.jsxPragmaFrag || 'Fragment'
   }
 
-  const opts = {
+  const options = {
     presetReact: {
       pragma: jsx.pragma,
-      pragmaFrag: jsx.fragment,
-      useBuiltIns: false
+      pragmaFrag: jsx.fragment
     },
     presetTypescript: {
       jsxPragma: jsx.pragma
@@ -31,24 +32,36 @@ export default (context?: CliContext, settings: Settings = {}) => {
 
   const config = {
     presets: [
-      [require.resolve('@babel/preset-react'), opts.presetReact],
-      [require.resolve('@babel/preset-typescript'), opts.presetTypescript]
+      // Support JSX syntax and React dev tools if required
+      // https://babeljs.io/docs/en/babel-preset-react
+      [require.resolve('@babel/preset-react'), options.presetReact],
+      // Parse and transform TypeScript without performing type checking
+      // https://babeljs.io/docs/en/next/babel-preset-typescript.html
+      [require.resolve('@babel/preset-typescript'), options.presetTypescript]
     ],
     plugins: [
-      [require.resolve('@babel/plugin-proposal-class-properties'), opts.pluginClassProperties],
-      [require.resolve('@babel/plugin-proposal-object-rest-spread'), opts.pluginObjectRestSpread],
-      [require.resolve('@babel/plugin-syntax-dynamic-import'), opts.pluginDynamicImport],
-      [require.resolve('@babel/plugin-transform-runtime'), opts.pluginTransformRuntime]
+      // Stage 3 proposal
+      // https://github.com/tc39/proposal-class-fields
+      [require.resolve('@babel/plugin-proposal-class-properties'), options.pluginClassProperties],
+      // Stage 4 proposal
+      // https://github.com/tc39/proposal-object-rest-spread
+      [require.resolve('@babel/plugin-proposal-object-rest-spread'), options.pluginObjectRestSpread],
+      // Enable Babel's built-in 'dynamicImport' flag which defines import() function usage
+      // https://babeljs.io/docs/en/next/babel-plugin-syntax-dynamic-import.html
+      [require.resolve('@babel/plugin-syntax-dynamic-import'), options.pluginDynamicImport],
+      // Enables the re-use of Babel helpers to save on codesize
+      // https://babeljs.io/docs/en/next/babel-plugin-transform-runtime.html
+      [require.resolve('@babel/plugin-transform-runtime'), options.pluginTransformRuntime]
     ]
   }
 
   if (context) {
-    context.amend('babelConfig::preset::react::options', opts.presetReact)
-    context.amend('babelConfig::preset::typescript::options', opts.presetTypescript)
-    context.amend('babelConfig::plugin::proposalClassProperties::options', opts.pluginClassProperties)
-    context.amend('babelConfig::plugin::proposalObjectRestSpread::options', opts.pluginObjectRestSpread)
-    context.amend('babelConfig::plugin::syntaxDynamicImport::options', opts.pluginDynamicImport)
-    context.amend('babelConfig::plugin::transformRuntime::options', opts.pluginTransformRuntime)
+    context.amend('babelConfig::preset::react::options', options.presetReact)
+    context.amend('babelConfig::preset::typescript::options', options.presetTypescript)
+    context.amend('babelConfig::plugin::proposalClassProperties::options', options.pluginClassProperties)
+    context.amend('babelConfig::plugin::proposalObjectRestSpread::options', options.pluginObjectRestSpread)
+    context.amend('babelConfig::plugin::syntaxDynamicImport::options', options.pluginDynamicImport)
+    context.amend('babelConfig::plugin::transformRuntime::options', options.pluginTransformRuntime)
   }
 
   return config
--- packages/anvil-plugin-ft-js/src/index.ts
+++ packages/anvil-plugin-ft-js/src/index.ts
@@ -1,36 +1,32 @@
 import { Plugin } from 'adonai'
 import babelPreset from './babel'
-import { Settings } from './types'
-import { RunningBabelContext } from '@financial-times/anvil-types-build'
+import { RunningWebpackContext, RunningBabelContext } from '@financial-times/anvil-types-build'
+import { PluginSettings } from './types'
 
-export default (settings: Settings = {}) => {
-  return new Plugin(({ on }) => {
-    on('@Build::amend::babelConfig', addPresetToBabelConfig(settings))
-    on('@Build::amend::babelConfig::preset::env::options', amendBabelPresetEnvOptions(settings))
-    on('@Build::amend::webpackConfig::scriptsRule', useCacheDirInScriptsRule)
-    on('@Build::amend::webpackConfig', amendWebpackConfig)
-  })
+export default new Plugin(({ on }) => {
+  on('@Build::amend::babelConfig', amendBabelConfig)
+  on('@Build::amend::babelConfig::preset::env::options', amendBabelPresetEnvOptions)
+  on('@Build::amend::webpackConfig::scriptsRule', amendWebpackConfigScriptsRule)
+  on('@Build::amend::webpackConfig', amendWebpackConfig)
+})
+
+function amendWebpackConfig({ webpackConfig }: RunningWebpackContext) {
+  // Add TypeScript files to the list of file extensions to resolve
+  webpackConfig.resolve.extensions.push('.ts', '.tsx')
 }
 
-function useCacheDirInScriptsRule({ scriptsRule }) {
+function amendWebpackConfigScriptsRule({ scriptsRule }) {
+  // Replace default JS rule matcher with a RegExp including TypeScript files
   scriptsRule.test = /\.(js|jsx|mjs|ts|tsx)$/
+  // Enable Babel caching to avoid running recompilation for each run
   scriptsRule.use.options.cacheDirectory = true
 }
 
-function addPresetToBabelConfig(settings) {
-  return ({ context, babelConfig }: RunningBabelContext) => {
-    babelConfig.presets.push(babelPreset(context, settings))
-  }
+function amendBabelConfig({ context, babelConfig }: RunningBabelContext) {
+  babelConfig.presets.push(babelPreset(context))
 }
 
-function amendBabelPresetEnvOptions(settings: Settings) {
-  return ({ options }) => ({
-    ...options,
-    targets: settings.envTargets || '> 1%, ie 11, bb 10',
-    useBuiltIns: true
-  })
-}
-
-function amendWebpackConfig({ webpackConfig }) {
-  webpackConfig.resolve.extensions.push('.ts', '.tsx')
+function amendBabelPresetEnvOptions({ context, options }) {
+  const settings: PluginSettings = context ? context.config.settings['ft-js'] : {}
+  options.targets = settings.presetEnvTargets || '> 1%, ie 11, bb 10'
 }
