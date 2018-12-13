33932af66945dcb4b46c95472b72a128e7a559cb
Maggie Allen <maggie.allen@ft.com>
2018-12-13 17:28:59 +0000

    extract client-side bootstrap.js to separate file; implement cuts-the-mustard; extends readme

--- packages/anvil-ui-ft-shell/README.md
+++ packages/anvil-ui-ft-shell/README.md
@@ -1,9 +1,40 @@
 # @financial-times/anvil-ui-ft-shell
 
-...
+This module provides a basic HTML document structure and client-side JavaScript bootstrap for ft.com apps.
+
 
 ## Installation
 
 ```
-npm install --save @financial-times/anvil-ui-ft-header
+npm install --save @financial-times/anvil-ui-ft-shell
 ```
+
+
+## Usage
+
+TBD
+
+
+## Client-side bootstrap
+
+Code which is executed immediately by the browser to deliver the JavaScript and styles required by the page.
+
+### JS/No JS
+
+The shell HTML has the default class `no-js`. If JavaScript is available the class will be replaced with `js`.
+
+### Enhanced/Core
+
+The shell HTML has the default class `core`. If the browser passes the cuts the mustard test the class will be replaced with `enhanced`.
+
+#### Cuts the mustard
+
+Cuts the mustard is a function which uses feature detection to determine if a browser is capable of supporting the JavaScript-enhanced experience.
+
+#### Script loading
+
+When serving an enhanced experience the configured script bundles will be asyncronously loaded and executed in order.
+
+#### Polyfill service
+
+The [Polyfill service](https://polyfill.io) is included on both core and enhanced experiences.
--- packages/anvil-ui-ft-shell/src/index.tsx
+++ packages/anvil-ui-ft-shell/src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { AnyObject } from '@financial-times/anvil-types-generic'
+import fs from 'fs'
+import path from 'path'
+
+const bootstrap = fs.readFileSync(path.join(__dirname, 'bootstrap.js')).toString()
 
 interface Props {
   children: any
@@ -8,58 +12,25 @@ interface Props {
 }
 
 export default function Shell({ children, scriptsToLoad = [], initialProps = {} }: Partial<Props>) {
-  const stringifiedInitialProps = JSON.stringify(initialProps)
-
-  // prettier-ignore
   return (
     <html className="no-js core">
       <head>
         <meta charSet="utf-8" />
-        <title>Page...</title>
+        <title>{/* TODO Configurable title text */}Financial Times</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script dangerouslySetInnerHTML={{ __html: `
-          var doc = document.documentElement
-
-          // For hydration later on
-          window.initialProps = JSON.parse('${stringifiedInitialProps}')
-
-          // Flag that JS is available
-          doc.className = doc.className.replace('no-js', 'js')
-
-          function scriptLoadError(error) {
-            // Log the script failure somehow
-            console.error(error)
-          }
-
-          function loadScript(src) {
-            var script = document.createElement('script')
-            script.onerror = scriptLoadError
-            script.async = false
-            script.src = src
-            document.head.appendChild(script)
-          }
-
-          // Cut the mustard like it's 2012
-          var enhanced = (function() {
-            return 'querySelector' in doc && 'classList' in doc
-          })()
-
-          if (enhanced) {
-            // flag that we will now try to deliver an ehanced experience
-            doc.className = doc.className.replace('core', 'enhanced')
-
-            const scripts = [
-              'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,HTMLPictureElement,fetch',
-              ${scriptsToLoad.map(script => `'${script}'`).join(',\n\t\t\t\t')}
-            ]
-
-            scripts.forEach(function(script) {
-              loadScript(script)
-            })
-          } else {
-            loadScript('https://cdn.polyfill.io/v2/polyfill.min.js?features=HTMLPictureElement')
-          }
-        `}} />
+        {/* TODO: Metadata slot */}
+        <script
+          type="application/json"
+          id="initial-props"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(initialProps) }}
+        />
+        <script
+          type="application/json"
+          id="scripts-config"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(scriptsToLoad) }}
+        />
+        <script dangerouslySetInnerHTML={{ __html: bootstrap }} />
+        {/* TODO: Critical CSS */}
       </head>
       <body>{children}</body>
     </html>
