3750883d3d7f9c5d7bf7c0268bc1e2b0b8407ffb
Matt Hinchliffe <matt@maketea.co.uk>
2018-12-19 17:38:11 +0000

    Refactor css plugin to use mini-extract-css-plugin

--- packages/anvil-plugin-css/src/index.ts
+++ packages/anvil-plugin-css/src/index.ts
@@ -1,28 +1,40 @@
 import { Plugin } from 'adonai'
 import { RunningWebpackContext } from '@financial-times/anvil-types-build'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export default new Plugin(({ on }) => {
   on('@Build::amend::webpackConfig', amendWebpackConfig)
 })
 
 function amendWebpackConfig({ context, webpackConfig }: RunningWebpackContext) {
+  const isDevMode = context.flags.devMode
+
+  const cssLoaderOptions = {}
+
+  const miniCssExtractPluginOptions = {
+    // only include content hash in filename when compiling production assets
+    filename: isDevMode ? '[name].css' : '[name].[contenthash:12].css'
+  }
+
   const cssRule = {
     test: /\.css$/,
     use: [
       {
-        loader: require.resolve('style-loader'),
-        options: {}
+        loader: MiniCssExtractPlugin.loader
       },
       {
         loader: require.resolve('css-loader'),
-        options: {
-          modules: 1
-        }
+        options: cssLoaderOptions
       }
     ]
   }
-  context.amend('webpackConfig::cssRule', cssRule)
-  context.amend('webpackConfig::cssRule::styleLoader', cssRule.use[0])
-  context.amend('webpackConfig::cssRule::cssLoader', cssRule.use[1])
+
+  context.amend('webpackConfig::cssPlugin', cssRule)
+  context.amend('webpackConfig::cssPlugin::cssLoaderOptions', cssLoaderOptions)
+  context.amend('webpackConfig::cssPlugin::miniCssExtractPluginOptions', miniCssExtractPluginOptions)
+
+  const miniCssExtractPlugin = new MiniCssExtractPlugin(miniCssExtractPluginOptions)
+
+  webpackConfig.plugins.push(miniCssExtractPlugin)
   webpackConfig.module.rules.push(cssRule)
 }
