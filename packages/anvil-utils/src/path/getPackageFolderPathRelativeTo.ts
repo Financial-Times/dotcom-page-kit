1e89eb24a1edbc4960eecab2878c40a7277c79c1
Matt Hinchliffe <matt@maketea.co.uk>
2018-12-20 10:50:26 +0000

    Remove unused anvil utils package

--- packages/anvil-utils/src/path/getPackageFolderPathRelativeTo.ts
+++
@@ -1,7 +0,0 @@
-import findUp from 'find-up'
-
-export function getPackageFolderPathRelativeTo(contextFolder) {
-  const packageDotJsonPath = findUp.sync('package.json', { cwd: contextFolder })
-  const filePath = packageDotJsonPath.replace('/package.json', '')
-  return filePath
-}
