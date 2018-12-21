65f1c00bb24a0494a8a501b79e8d3ba29633ab2f
Matt Hinchliffe <matt@maketea.co.uk>
2018-12-21 11:00:30 +0000

    Add webpack-fix-style-only-entries plugin to css build plugin to ignore JS output for CSS entry points

--- packages/anvil-plugin-css/src/index.ts
+++ packages/anvil-plugin-css/src/index.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'adonai'
 import { RunningWebpackContext } from '@financial-times/anvil-types-build'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import StylesOnlyPlugin from 'webpack-fix-style-only-entries'
 
 export default new Plugin(({ on }) => {
   on('@Build::amend::webpackConfig', amendWebpackConfig)
@@ -29,6 +30,12 @@ function amendWebpackConfig({ context, webpackConfig }: RunningWebpackContext) {
   context.amend('webpackConfig::cssPlugin::rule', cssRule)
 
   webpackConfig.module.rules.push(cssRule)
+
+  const stylesOnlyPluginOptions = {}
+
+  context.amend('webpackConfig::cssPlugin::stylesOnlyPluginOptions', stylesOnlyPluginOptions)
+
+  webpackConfig.plugins.push(new StylesOnlyPlugin(stylesOnlyPluginOptions))
 
   const cssExtractPluginOptions = {
     // only include content hash in filename when compiling production assets
