fec06e3e085aca2be4d69dddeeb69e305ee00b92
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-01-03 12:45:30 +0000

    Express handlebars demo (#82)
    * Setup Handlebars renderer in example app
    
    * Setup rendering using n-handlebars
    
    * Setup demo using Handlebars template inheritence
    
    * Add helper function to example app handlebars renderer to format block output

---
+++ examples/express-app/server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 3456
+
+app.locals.siteTitle = 'Example App'
+
+app.use(require('./middleware/anvil'))
+
+app.get('/', require('./controllers/home'))
+
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening on PORT:${port}`)
+})
---
+++ examples/express-app/server/controllers/home.js
@@ -0,0 +1,18 @@
+const render = require('../lib/render')
+
+module.exports = async (request, response) => {
+  const data = {
+    pageTitle: 'Home',
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  }
+
+  try {
+    const { html } = await render('home', { ...request.app.locals, ...response.locals, ...data })
+    response.send(html)
+  } catch (error) {
+    console.error(error)
+    response.send(500, 'Oh no, something went wrong!')
+  }
+}
