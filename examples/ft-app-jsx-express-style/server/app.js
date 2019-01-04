5e6f770bdfdb5f9a2d5b3e38d0d1e0c28b0f96d1
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-01-04 17:10:26 +0000

    Refactor asset loader out of renderer (#86)
    * Refactor the asset loader out of the prototype jsx renderer so that file names may be specified by the consuming application
    
    * Refactor nextjs style demo to use asset loader directly
    
    * Refactor express style demo to use asset loader directly

--- examples/ft-app-jsx-express-style/server/app.js
+++ examples/ft-app-jsx-express-style/server/app.js
@@ -2,17 +2,22 @@ import path from 'path'
 import express from 'express'
 import { homePageController } from './controllers/home'
 import { aboutPageController } from './controllers/about'
+import AssetLoader from '@financial-times/anvil-server-asset-loader'
 import { createRendererMiddleware } from '@financial-times/anvil-server-jsx-renderer/express'
 
 export const app = express()
 
-const renderer = createRendererMiddleware({
-  assetUrlPrefix: process.env.ASSET_URL_PREFIX || '/assets',
-  assetManifestPath: path.join(__dirname, '../dist/manifest.json')
+const assets = new AssetLoader({
+  fileSystemPath: './dist',
+  publicPath: '/assets'
 })
 
+const scriptsToLoad = [assets.getPublicPath('client.js'), assets.getPublicPath('runtime.js')]
+
+const renderer = createRendererMiddleware({ scriptsToLoad })
+
 app.use(renderer)
-app.use('/assets', express.static(path.join(__dirname, '../dist')))
+app.use('/assets', express.static(path.resolve('./dist')))
 
 app.get('/', homePageController)
 app.get('/about', aboutPageController)
