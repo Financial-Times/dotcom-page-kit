bddfd5f2dbc26726f08dface7eef19c9f7749490
Matt Hinchliffe <matt@maketea.co.uk>
2019-01-09 15:27:30 +0000

    Add Express typings to A/B test middleware

--- packages/anvil-middleware-ft-ab-state/src/index.ts
+++ packages/anvil-middleware-ft-ab-state/src/index.ts
@@ -1,7 +1,8 @@
 import TestStatus from './TestStatus'
+import { Handler, Request, Response, NextFunction } from 'express'
 
-export const init = () => {
-  return (request, response, next) => {
+export const init = (): Handler => {
+  return (request: Request, response: Response, next: NextFunction) => {
     /**
      * An 'ft-ab' header is added to all requests during preflight
      */
