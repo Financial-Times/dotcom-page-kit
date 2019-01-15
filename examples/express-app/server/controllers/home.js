4c93f79e6368d7a52d35a115a3344606ec2cf311
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-01-15 12:25:31 +0000

    FT Handlebars package (#77)
    * Abstract n-handlebars setup from example app into a separate package
    
    * Ensure full path to the requested view file is resolved before calling express-handlebars directly
    
    * Use express-handlebars directly for rendering to avoid doubling up partial loading with n-handlebars
    
    * Enable caching of partials to avoid slow filesystem i/o for each render call
    
    * Turn down athloi concurrency to avoid hitting disk access limits
    
    * Expose express-handlebars mount point in ft-handlebars module so it may be used as an Express view engine
    
    * Simplift ft-handlebars modules to expose the same interface as underlying express-handlebars module
    
    * Add readme to FT Handlebars package
    
    * Improve documentation about partial and layout template lookups

--- examples/express-app/server/controllers/home.js
+++ examples/express-app/server/controllers/home.js
@@ -1,11 +1,10 @@
-const render = require('../lib/render')
-
 module.exports = async (request, response) => {
   const options = {
     pageTitle: 'Home',
     foo: 'foo',
     bar: 'bar',
     baz: 'baz',
+    layout: 'main',
     scripts: [response.locals.assets.loader.getPublicURL('main.js')]
   }
 
@@ -14,12 +13,7 @@ module.exports = async (request, response) => {
   })
 
   try {
-    // When calling Express's `.render()` method locals will be appended to view data.
-    // <https://github.com/expressjs/express/blob/master/lib/application.js#L545-L554>
-    const data = { ...request.app.locals, ...response.locals, ...options }
-    const output = await render('home', data)
-
-    response.send(output.html)
+    response.render('home.hbs', options)
   } catch (error) {
     console.error(error) // eslint-disable-line no-console
     response.status(500).send('Oh no, something went wrong!')
