696cf758c53c783d978c0ef1d5a85d4b75a735f3
Maggie Allen <maggie.allen@ft.com>
2019-01-21 11:34:12 +0000

    adds an ft-header package with storybook

--- packages/anvil-ui-ft-header/src/story.tsx
+++ packages/anvil-ui-ft-header/src/story.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import FTHeader from '.'
+import { Header } from '.'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs, radios } from '@storybook/addon-knobs'
+import { OrigamiBuildService } from '@financial-times/anvil-ui-origami-build-service'
+import storyData from './storyData'
+
+const userStateOptions = ['User state', { user: true, anon: null }, 'true']
 
 storiesOf('FT / Header', module)
   .addDecorator(withKnobs)
-  .add('default', () => {
-    const someText = text('someText', 'foo')
-    return <FTHeader>{someText}</FTHeader>
+  .add('header', () => {
+    const toggleUserState = radios(...userStateOptions)
+    return (
+      <OrigamiBuildService
+        dependencies={{
+          'o-header': '^7.7.0',
+          'o-fonts': '^3.2.0',
+          'o-normalise': '^1.6.2'
+        }}>
+        <Header {...storyData} userNav={toggleUserState} />
+      </OrigamiBuildService>
+    )
   })
