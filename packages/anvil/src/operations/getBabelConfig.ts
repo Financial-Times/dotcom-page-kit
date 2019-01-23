6e5832aecfdf746b6f28ee95780d89687841c4af
Ifeanyi Isitor <33205310+ifeanyiisitor@users.noreply.github.com>
2019-01-23 16:16:04 +0000

    Add @babel/preset-env defaults (#102)

--- packages/anvil/src/operations/getBabelConfig.ts
+++ packages/anvil/src/operations/getBabelConfig.ts
@@ -7,8 +7,16 @@ import { CliContext } from '../entities/CliContext'
  * to construct a preset that can be specified in a .babelrc file. When used in as a preset,
  * there will be no args supplied to the function, hence why the `cli` arg is optional.
  */
+
 export function getBabelConfig(cli?: CliContext) {
-  const presetEnvOpts = {}
+  const defaultTargets = '> 1%, ie 11, bb 10, ff ESR'
+  const presetEnvOpts = {
+    targets: cli.config.settings.targets || defaultTargets,
+    useBuiltIns: false,
+    // Exclude transforms that make all code slower
+    // See https://github.com/facebook/create-react-app/pull/5278
+    exclude: ['transform-typeof-symbol']
+  }
 
   const babelConfig = {
     presets: [[require.resolve('@babel/preset-env'), presetEnvOpts]],
