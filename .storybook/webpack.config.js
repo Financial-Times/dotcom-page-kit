e381a8b67bc0e9c9a79d2610582152132937e0b2
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-01-31 17:58:04 +0000

    Disable Webpack resolving symlinks to their true paths so that cross-dependencies can be ignored correctly from transpilation

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,17 @@
+// This configuration extends the existing Storybook Webpack config.
+// See https://storybook.js.org/configurations/custom-webpack-config/ for more info.
+// The default Webpack and Babel configs can be seen here:
+// - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/preview/preview-preset.js
+// - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/common/babel.js
 module.exports = {
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    symlinks: false,
+    extensions: ['.ts', '.tsx']
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(ts|tsx)$/,
         loader: require.resolve('babel-loader'),
         options: {
           presets: [['react-app', { flow: false, typescript: true }]]
