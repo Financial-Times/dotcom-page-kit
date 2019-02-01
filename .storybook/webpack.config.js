a7de8e3e8f725723b4f6765313f4d2bf45add4b9
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-02-01 12:15:48 +0000

    Add more generic exclude patterns to Storybook webpack config to prevent dependencies being transpiled twice

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,33 @@
 // This configuration extends the existing Storybook Webpack config.
 // See https://storybook.js.org/configurations/custom-webpack-config/ for more info.
+//
 // The default Webpack and Babel configs can be seen here:
 // - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/preview/preview-preset.js
 // - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/common/babel.js
-module.exports = {
-  resolve: {
-    symlinks: false,
-    extensions: ['.ts', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [['react-app', { flow: false, typescript: true }]]
-        }
-      }
-    ]
-  }
+
+const excludePaths = [/node_modules/, /dist/]
+
+module.exports = (baseConfig) => {
+  // Use real file paths for symlinked dependencies do avoid including them multiple times
+  baseConfig.resolve.symlinks = true
+
+  // HACK: extend existing JS rule to ensure all dependencies are correctly ignored
+  // https://github.com/storybooks/storybook/issues/3346#issuecomment-459439438
+  const jsRule = baseConfig.module.rules.find((rule) => rule.test.test('.jsx'))
+  jsRule.exclude = excludePaths
+
+  // Add support for TypeScript source code
+  // https://storybook.js.org/configurations/typescript-config/
+  baseConfig.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]]
+    },
+    exclude: excludePaths
+  })
+
+  baseConfig.resolve.extensions.push('.ts', '.tsx')
+
+  return baseConfig
 }
