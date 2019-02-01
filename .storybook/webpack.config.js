93a710ac09258368b7aad75ed368ac3d3d929494
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2019-02-01 12:43:34 +0000

    Refactor Storybook config to use babel-loader for all source files (#112)
    * Refactor Storybook config to use babel-loader for all source files
    
    This swaps awesome-typescript-loader for babel-loader using the react-app preset which ships with Storybook.
    The change was required due to the TypeScript loader using its own module resolution algorithm which was not
    fully extensible via Webpack leading to us being unable to use packages installed via Bower.
    
    * Disable Webpack resolving symlinks to their true paths so that cross-dependencies can be ignored correctly from transpilation
    
    * Add more generic exclude patterns to Storybook webpack config to prevent dependencies being transpiled twice

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,33 @@
-module.exports = {
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
-      }
-    ]
-  }
+// This configuration extends the existing Storybook Webpack config.
+// See https://storybook.js.org/configurations/custom-webpack-config/ for more info.
+//
+// The default Webpack and Babel configs can be seen here:
+// - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/preview/preview-preset.js
+// - https://github.com/storybooks/storybook/blob/release/4.2/lib/core/src/server/common/babel.js
+
+const excludePaths = [/node_modules/, /dist/]
+
+module.exports = (baseConfig) => {
+  // Use real file paths for symlinked dependencies do avoid including them multiple times
+  baseConfig.resolve.symlinks = true
+
+  // HACK: extend existing JS rule to ensure all dependencies are correctly ignored
+  // https://github.com/storybooks/storybook/issues/3346#issuecomment-459439438
+  const jsRule = baseConfig.module.rules.find((rule) => rule.test.test('.jsx'))
+  jsRule.exclude = excludePaths
+
+  // Add support for TypeScript source code
+  // https://storybook.js.org/configurations/typescript-config/
+  baseConfig.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]]
+    },
+    exclude: excludePaths
+  })
+
+  baseConfig.resolve.extensions.push('.ts', '.tsx')
+
+  return baseConfig
 }
