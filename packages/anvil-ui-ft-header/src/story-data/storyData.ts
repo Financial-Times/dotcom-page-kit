9b3224768bca226ca27798d9e251b2ac607dfc81
Maggie Allen <maggie.allen@ft.com>
2019-02-06 15:25:45 +0000

    enable  data imports as json

--- packages/anvil-ui-ft-header/src/story-data/storyData.ts
+++ packages/anvil-ui-ft-header/src/story-data/storyData.ts
@@ -1,26 +1,29 @@
-import navbarUk from './navbar-uk'
-import navbarSimple from './navbar-simple'
-import navbarRight from './navbar-right'
-import navbarRightAnon from './navbar-right-anon'
-import crumbtrail from './crumbtrail-uk'
+import navbarUk from './navbarUk.json'
+import navbarSimple from './navbarSimple.json'
+import navbarRight from './navbarRight.json'
+import navbarRightAnon from './navbarRightAnon.json'
+import crumbtrail from './crumbtrailUk.json'
 
 const breadcrumb = crumbtrail.ancestors.concat(crumbtrail.item)
 const subsections = crumbtrail.children
 
 export default {
+  // Options
   userNav: false,
   disableStick: false,
   variant: '',
   hideOutboundLinks: false,
-  navbar: navbarUk,
   viewStyle: 'compact',
   userIsAnonymous: false,
   userIsLoggedIn: true,
+  showSubNav: true,
+  showSignOut: true,
+
+  // Data
+  navbar: navbarUk,
   'navbar-right': navbarRight,
   'navbar-right-anon': navbarRightAnon,
   'navbar-simple': navbarSimple,
   breadcrumb: breadcrumb,
-  subsections: subsections,
-  showSubNav: true,
-  showSignOut: true
+  subsections: subsections
 }
