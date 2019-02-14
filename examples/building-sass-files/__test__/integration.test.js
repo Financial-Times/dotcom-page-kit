1ca21bb2dad5e35e79839715ccc5377dfee80f9b
Matt Hinchliffe <matt@maketea.co.uk>
2019-02-14 12:36:59 +0000

    Do not test examples using top-level test command or build examples on CI pending proper solution

--- examples/building-sass-files/__test__/integration.test.js
+++ examples/building-sass-files/__test__/integration.test.js
@@ -4,7 +4,7 @@ const path = require('path')
 const outputFile = path.join(__dirname, '../dist/styles.css')
 const outputContents = fs.readFileSync(outputFile).toString()
 
-describe('examples/build-sass', () => {
+describe('examples/building-sass-files', () => {
   it('compiles Sass files to CSS file', () => {
     expect(outputContents).toMatchSnapshot()
   })
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testPathIgnorePatterns: ['/bower_components/', '/node_modules/']
+  testPathIgnorePatterns: ['/bower_components/', '/node_modules/', '/examples/']
 }
