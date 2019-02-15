875843d9294c29090c3e30646f4b85f1259bb1e5
Matt Hinchliffe <matt@maketea.co.uk>
2019-02-15 08:44:28 +0000

    Add more specific tests to express handlebars demo

--- examples/express-with-handlebars/__test__/integration.test.js
+++ examples/express-with-handlebars/__test__/integration.test.js
@@ -2,8 +2,33 @@ const request = require('supertest')
 const app = require('../server/app')
 
 describe('examples/express-with-handlebars', () => {
-  it('renders the page', async () => {
-    const response = await request(app).get('/')
+  let response
+
+  beforeEach(async () => {
+    response = await request(app).get('/')
+  })
+
+  it('returns an OK response', () => {
     expect(response.statusCode).toBe(200)
+  })
+
+  it('renders a view', () => {
+    expect(response.text).toContain('<h1 class="Page-title">')
+  })
+
+  it('renders view partials', () => {
+    expect(response.text).toContain('<dl class="DogList">')
+  })
+
+  it('renders the view into a layout', () => {
+    expect(response.text).toContain('<html lang="en">')
+  })
+
+  it('renders layout partials ', () => {
+    expect(response.text).toContain('<div class="Header">')
+  })
+
+  it('supports template inheritance', () => {
+    expect(response.text).toContain('<meta name="description"')
   })
 })
