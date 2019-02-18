ba058e3adcfb13834f1a905bdb806ceb9c8fd8a8
Maggie Allen <maggie.allen@ft.com>
2019-02-18 17:49:06 +0000

    header rendering in demo with navigation-middleware data

--- examples/express-ft-header/server/middleware/anvil.js
+++ examples/express-ft-header/server/middleware/anvil.js
@@ -1,1 +1,7 @@
-module.exports = []
+const editionMiddleware = require('@financial-times/anvil-middleware-ft-edition')
+const navigationMiddleware = require('@financial-times/anvil-middleware-ft-navigation')
+
+const edition = editionMiddleware.init()
+const navigation = navigationMiddleware.init({ enableCrumbtrail: true })
+
+module.exports = [edition, navigation]
