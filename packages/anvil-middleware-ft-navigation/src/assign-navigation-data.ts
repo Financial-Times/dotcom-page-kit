acdfde3130fbabef9dde6809b4d815f60992fec7
Maggie Allen <maggie.allen@ft.com>
2019-02-20 12:06:38 +0000

    add deep-freeze to make the updated navigation data immutable

--- packages/anvil-middleware-ft-navigation/src/assign-navigation-data.ts
+++ packages/anvil-middleware-ft-navigation/src/assign-navigation-data.ts
@@ -1,15 +1,23 @@
+import deepFreeze from 'deep-freeze'
+
 const addEditionBasedProperties = (navigation, currentEdition) => {
-  const clone = JSON.parse(JSON.stringify(navigation))
-  clone.navbar = currentEdition === 'uk' ? 'navbar-uk' : 'navbar-international'
-  clone.drawer = currentEdition === 'uk' ? 'drawer-uk' : 'drawer-international'
-  return clone
+  // Navigation data from the anvil-ft-server-navigation package is frozen
+  // and must be cloned before modification
+  const clonedNavigation = JSON.parse(JSON.stringify(navigation))
+  clonedNavigation.navbar = currentEdition === 'international' ? 'navbar-international' : 'navbar-uk'
+  clonedNavigation.drawer = currentEdition === 'international' ? 'drawer-international' : 'drawer-uk'
+
+  return clonedNavigation
 }
 
 export const assignNavigation = (navigation, editions) => {
   const currentEdition = editions.current.id
-  let response
-  if(currentEdition && navigation) {
-    response = addEditionBasedProperties(navigation, editions)
+
+  if (currentEdition && navigation) {
+    const response = addEditionBasedProperties(navigation, editions)
+    // Re-freezes the navigation data making it completely immutable
+    return deepFreeze(response)
   }
-  return response
+
+  return navigation
 }
