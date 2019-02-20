a36721fc383b01876649ac31f5859c59a9aea6e4
Maggie Allen <maggie.allen@ft.com>
2019-02-20 10:15:40 +0000

    remove crumbtrail option from middleware initialization

--- examples/express-ft-header/server/middleware/anvil.js
+++ examples/express-ft-header/server/middleware/anvil.js
@@ -2,6 +2,6 @@ const editionMiddleware = require('@financial-times/anvil-middleware-ft-edition'
 const navigationMiddleware = require('@financial-times/anvil-middleware-ft-navigation')
 
 const edition = editionMiddleware.init()
-const navigation = navigationMiddleware.init({ enableCrumbtrail: true })
+const navigation = navigationMiddleware.init()
 
 module.exports = [edition, navigation]
