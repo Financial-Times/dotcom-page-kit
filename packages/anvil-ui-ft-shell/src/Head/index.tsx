e1a200f3b143ae751007f225b91a2b526d39f688
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-02-21 15:17:34 +0000

    Add basic document head component

---
+++ packages/anvil-ui-ft-shell/src/Head/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const HtmlHead = (props) => (
+  <React.Fragment>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>{props.pageTitle ? `${props.pageTitle} | ${props.siteTitle}` : props.siteTitle}</title>
+
+    <meta name="description" content={props.description || props.summary} />
+
+    <link rel="canonical" href={props.url} />
+
+    <meta name="robots" content={props.robots} />
+
+    <meta name="google-site-verification" content={props.googleSiteVerification} />
+
+    {props.children}
+  </React.Fragment>
+)
+
+HtmlHead.defaultProps = {
+  description: 'News, analysis and comment from the Financial Times, the worldʼs leading global business publication',
+  robots: 'index,follow',
+  siteTitle: 'Financial Times',
+  googleSiteVerification: '4-t8sFaPvpO5FH_Gnw1dkM28CQepjzo8UjjAkdDflTw'
+}
+
+export default HtmlHead
