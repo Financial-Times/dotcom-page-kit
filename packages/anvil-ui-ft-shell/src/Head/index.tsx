5a7d41194db01861090d001ecdfc2b5b330e9d7c
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-02-21 15:21:51 +0000

    Add resource hints to head component

--- packages/anvil-ui-ft-shell/src/Head/index.tsx
+++ packages/anvil-ui-ft-shell/src/Head/index.tsx
@@ -7,13 +7,17 @@ const HtmlHead = (props) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <title>{props.pageTitle ? `${props.pageTitle} | ${props.siteTitle}` : props.siteTitle}</title>
-
     <meta name="description" content={props.description || props.summary} />
-
     <link rel="canonical" href={props.url} />
 
-    <meta name="robots" content={props.robots} />
+    {/* resource hints */}
+    <link rel="preconnect" href="https://spoor-api.ft.com" />
+    <link rel="preconnect" href="https://session-next.ft.com" crossorigin="use-credentials" />
+    <link rel="preconnect" href="https://ads-api.ft.com" />
+    <link rel="preconnect" href="https://www.googletagservices.com" />
 
+    {/* SEO */}
+    <meta name="robots" content={props.robots} />
     <meta name="google-site-verification" content={props.googleSiteVerification} />
 
     {props.children}
@@ -21,7 +25,8 @@ const HtmlHead = (props) => (
 )
 
 HtmlHead.defaultProps = {
-  description: 'News, analysis and comment from the Financial Times, the worldʼs leading global business publication',
+  description:
+    'News, analysis and comment from the Financial Times, the worldʼs leading global business publication',
   robots: 'index,follow',
   siteTitle: 'Financial Times',
   googleSiteVerification: '4-t8sFaPvpO5FH_Gnw1dkM28CQepjzo8UjjAkdDflTw'
