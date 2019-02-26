2343baaa0675b20b4f42837146b355ad87210b81
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-02-26 16:44:38 +0000

    improve footer storybook radios and origami options

--- packages/anvil-ui-ft-footer/src/story.tsx
+++ packages/anvil-ui-ft-footer/src/story.tsx
@@ -5,33 +5,27 @@ import { withKnobs, radios } from '@storybook/addon-knobs'
 import { OrigamiBuildService } from '@financial-times/anvil-ui-origami-build-service'
 import sampleData from './storyData'
 
-const themeOptions = ['Theme', { dark: 'dark', light: 'light' }, 'dark']
+const toggleTheme = () => radios('Theme', { dark: 'dark', light: 'light' }, 'dark')
+
+const OrigamiDependecies = {
+  'o-footer': '^6.0.10',
+  'o-fonts': '^3.2.0',
+  'o-normalise': '^1.6.2'
+}
 
 storiesOf('FT / Footer', module)
   .addDecorator(withKnobs)
   .add('footer', () => {
-    const toggleTheme = radios(...themeOptions)
     return (
-      <OrigamiBuildService
-        dependencies={{
-          'o-footer': '^6.0.10',
-          'o-fonts': '^3.2.0',
-          'o-normalise': '^1.6.2'
-        }}>
-        <Footer {...sampleData} theme={toggleTheme} />
+      <OrigamiBuildService dependencies={OrigamiDependecies}>
+        <Footer {...sampleData} theme={toggleTheme()} />
       </OrigamiBuildService>
     )
   })
   .add('legal footer', () => {
-    const toggleTheme = radios(...themeOptions)
     return (
-      <OrigamiBuildService
-        dependencies={{
-          'o-footer': '^6.0.10',
-          'o-fonts': '^3.2.0',
-          'o-normalise': '^1.6.2'
-        }}>
-        <LegalFooter {...sampleData} theme={toggleTheme} />
+      <OrigamiBuildService dependencies={OrigamiDependecies}>
+        <LegalFooter {...sampleData} theme={toggleTheme()} />
       </OrigamiBuildService>
     )
   })
