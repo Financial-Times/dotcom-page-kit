deda06e1cb49e84a7163ff4c8b4150bc10869c67
Maggie Allen <maggie.allen@ft.com>
2019-02-28 11:12:54 +0000

    housekeeping

--- packages/anvil-ui-ft-header/src/components/drawer/additionalPartials.tsx
+++ packages/anvil-ui-ft-header/src/components/drawer/additionalPartials.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import * as utils from './utils'
 
 export const DrawerParentItem = ({ label, url, submenu, index, equalsCurrentUrl }) => {
-  // TODO adds types to classes dynamically
   return (
     <React.Fragment>
       <div key={url} className="o-header__drawer-menu-toggle-wrapper">
@@ -22,7 +21,7 @@ export const DrawerParentItem = ({ label, url, submenu, index, equalsCurrentUrl 
       </div>
       <ul
         className="o-header__drawer-menu-list o-header__drawer-menu-list--child"
-        id={'o-header-drawer-child-' + index}
+        id={`o-header-drawer-child-${index}`}
         data-trackable="sub-level">
         {submenu.items.map((item) => {
           return (
