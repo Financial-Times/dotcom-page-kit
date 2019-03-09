61a38827939944da8432c1b7701598ad62ccc34b
chee <chee@snoot.club>
2019-03-09 01:27:09 +0000

    Add browser entrypoints (purely passthru rn)

---
+++ packages/anvil-ui-ft-footer/browser.js
@@ -0,0 +1,16 @@
+import Footer from 'o-footer'
+
+/**
+ * @typedef FooterOptions
+ * @property {HTMLElement} [rootElement] the root element passed to o-footer
+ */
+
+/**
+ * Initialise the header
+ * @param {FooterOptions} footerOptions
+ */
+export const init = (footerOptions = {}) => {
+  Footer.init(footerOptions.rootElement)
+}
+
+export { Footer as OrigamiFooter }
---
+++ packages/anvil-ui-ft-header/browser.js
@@ -0,0 +1,16 @@
+import Header from 'o-header'
+
+/**
+ * @typedef HeaderOptions
+ * @property {HTMLElement} [rootElement] the root element passed to o-header
+ */
+
+/**
+ * Initialise the header
+ * @param {HeaderOptions} headerOptions
+ */
+export const init = (headerOptions = {}) => {
+  Header.init(headerOptions.rootElement)
+}
+
+export { Header as OrigamiHeader }
