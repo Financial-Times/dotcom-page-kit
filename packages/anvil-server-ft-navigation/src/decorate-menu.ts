5e525cbd75b3a595976adb1ac3539929aeb59d9b
Maggie Allen <maggie.allen@ft.com>
2019-03-11 13:27:12 +0000

    taking in PR comments

--- packages/anvil-server-ft-navigation/src/decorate-menu.ts
+++ packages/anvil-server-ft-navigation/src/decorate-menu.ts
@@ -12,22 +12,23 @@ const decorateUrl = (itemUrl, currentUrl, currentPathName) => {
 }
 
 const isSelected = (url, currentPathName) => {
-  return url === currentPathName ? true : false
+  return url === currentPathName
 }
 
-const decorateItem = (url, currentUrl, label) => {
+const decorateItem = (url, currentUrl) => {
   const currentPathName = parse(currentUrl).pathname
   const itemUrl = decorateUrl(url, currentUrl, currentPathName)
   let selected = isSelected(url, currentPathName)
-  return label ? { label, url: itemUrl, selected } : { itemUrl, selected }
+  return { itemUrl, selected }
 }
 
-export const processDataItems = (dataItems, currentUrl) => {
-  return Array.isArray(dataItems)
-    ? dataItems.map((dataItem) => {
-        return decorateItem(dataItem.url, currentUrl, dataItem.label)
-      })
-    : decorateItem(dataItems.url, currentUrl, dataItems.label)
+export const processDataItems = (dataItem, currentUrl) => {
+  if (Array.isArray(dataItem)) {
+    return dataItem.map((item) => processDataItems(item, currentUrl))
+  } else {
+    const { itemUrl, selected } = decorateItem(dataItem.url, currentUrl)
+    return { ...dataItem, url: itemUrl, selected }
+  }
 }
 
 export const processMeganav = (meganav, currentUrl) => {
@@ -51,12 +52,20 @@ export const decorateMenu = ({ label, items }: TNavMenu, currentUrl: string): TN
   return {
     label,
     items: items.reduce((acc, { label, url, submenu, meganav }: TNavMenuItem) => {
-      const { itemUrl, selected } = decorateItem(url, currentUrl, null)
+      const { itemUrl, selected } = decorateItem(url, currentUrl)
 
       if (submenu) {
         submenu = decorateMenu(submenu, currentUrl)
       }
 
+      /**
+       * Meganav data is appended to the navigation object by next-api
+       * and present in the response from anvil-server-ft-navigation.
+       *
+       * It can't be processed by decorateMenu() as it has an atypical structure.
+       * Note also that the nesting is variable and the .data property may contain
+       * dataItems or a nested array of dataItems.
+       */
       if (meganav) {
         meganav = processMeganav(meganav, currentUrl)
       }
