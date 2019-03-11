144a3fa7eae0d5bf0c33052026e3830a9c446819
Matt Hinchliffe <matt@maketea.co.uk>
2019-03-11 08:38:28 +0000

    Remove Bower example package as it is no longer needed

--- packages/anvil-ui-bower-test/src/clientside.js
+++
@@ -1,8 +0,0 @@
-import Toggle from 'o-toggle'
-
-export function init() {
-  const toggleButton = document.querySelector('.Expander-toggle')
-  const target = document.querySelector('.Expander-content')
-
-  return new Toggle(toggleButton, { target })
-}
