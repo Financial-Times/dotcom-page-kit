dbb9aeda53cf1bce735670f3a77a0ff18a16ed12
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-03-15 17:46:22 +0000

    Fixed issues with ssr-with-hydration test

--- examples/ssr-with-hydration/app/components/Page.js
+++ examples/ssr-with-hydration/app/components/Page.js
@@ -1,10 +1,28 @@
 import React from 'react'
+
+import { placeholder, createSlotterFor } from '@financial-times/anvil-ui-slots'
+
 import { PageNavigation } from './PageNavigation'
 
-export default function Page() {
+Page.Body = placeholder()
+Page.Header = placeholder()
+Page.Footer = placeholder()
+
+export default function Page(props) {
+  const Slot = createSlotterFor(Page, props)
+
   return (
     <React.Fragment>
-      <PageNavigation />
+      <header>
+        <Slot name="Header" />
+        <PageNavigation />
+      </header>
+      <main id="root">
+        <Slot name="Body" />
+      </main>
+      <footer>
+        <Slot name="Footer" />
+      </footer>
     </React.Fragment>
   )
 }
