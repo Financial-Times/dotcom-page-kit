61e4c0dbbb285d227150172ed51c3a953a177552
Matt Hinchliffe <matt@maketea.co.uk>
2019-04-10 11:09:37 +0100

    Configure Jest root to avoid it touching too many files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
+  roots: ['./packages'],
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testPathIgnorePatterns: ['/bower_components/', '/node_modules/', '/examples/']
+  testPathIgnorePatterns: ['/bower_components/', '/node_modules/']
 }
