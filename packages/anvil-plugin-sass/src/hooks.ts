83cffd866e3318513bc5b114d0f851facd2d088c
Matt Hinchliffe <matt@maketea.co.uk>
2019-04-17 09:19:14 +0100

    Refactor Sass plugin hooks into enum

--- packages/anvil-plugin-sass/src/hooks.ts
+++ packages/anvil-plugin-sass/src/hooks.ts
@@ -1,10 +1,10 @@
-export const hooks = {
-  SCSS_RULE: 'webpackConfig::sassPlugin::rule',
-  CSSNANO_OPTIONS: 'webpackConfig::sassPlugin::cssnanoOptions',
-  CSS_LOADER_OPTIONS: 'webpackConfig::sassPlugin::cssLoaderOptions',
-  SASS_LOADER_OPTIONS: 'webpackConfig::sassPlugin::sassLoaderOptions',
-  AUTOPREFIXER_OPTIONS: 'webpackConfig::sassPlugin::autoprefixerOptions',
-  POSTCSS_LOADER_OPTIONS: 'webpackConfig::sassPlugin::postcssLoaderOptions',
-  STYLES_ONLY_PLUGIN_OPTIONS: 'webpackConfig::sassPlugin::stylesOnlyPluginOptions',
-  MINI_CSS_EXTRACT_PLUGIN_OPTIONS: 'webpackConfig::sassPlugin::miniCssExtractPluginOptions'
+export enum hooks {
+  SCSS_RULE ='webpackConfig::sassPlugin::rule',
+  CSSNANO_OPTIONS ='webpackConfig::sassPlugin::cssnanoOptions',
+  CSS_LOADER_OPTIONS ='webpackConfig::sassPlugin::cssLoaderOptions',
+  SASS_LOADER_OPTIONS ='webpackConfig::sassPlugin::sassLoaderOptions',
+  AUTOPREFIXER_OPTIONS ='webpackConfig::sassPlugin::autoprefixerOptions',
+  POSTCSS_LOADER_OPTIONS ='webpackConfig::sassPlugin::postcssLoaderOptions',
+  STYLES_ONLY_PLUGIN_OPTIONS ='webpackConfig::sassPlugin::stylesOnlyPluginOptions',
+  MINI_CSS_EXTRACT_PLUGIN_OPTIONS ='webpackConfig::sassPlugin::miniCssExtractPluginOptions'
 }
