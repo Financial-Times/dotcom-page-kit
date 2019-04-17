2dff22de9fb80deb1df7042b4264c75f7111f0ea
Matt Hinchliffe <matt@maketea.co.uk>
2019-04-17 09:00:06 +0100

    Refactor CSS plugin hooks into enum

--- packages/anvil-plugin-css/src/hooks.ts
+++ packages/anvil-plugin-css/src/hooks.ts
@@ -1,6 +1,6 @@
-export const hooks = {
-  CSS_RULE: 'webpackConfig::cssPlugin::rule',
-  CSS_LOADER_OPTIONS: 'webpackConfig::cssPlugin::cssLoaderOptions',
-  MINI_CSS_EXTRACT_PLUGIN_OPTIONS: 'webpackConfig::cssPlugin::miniCssExtractPluginOptions',
-  STYLES_ONLY_PLUGIN_OPTIONS: 'webpackConfig::cssPlugin::stylesOnlyPluginOptions'
+export enum hooks {
+  CSS_RULE = 'webpackConfig::cssPlugin::rule',
+  CSS_LOADER_OPTIONS = 'webpackConfig::cssPlugin::cssLoaderOptions',
+  MINI_CSS_EXTRACT_PLUGIN_OPTIONS = 'webpackConfig::cssPlugin::miniCssExtractPluginOptions',
+  STYLES_ONLY_PLUGIN_OPTIONS = 'webpackConfig::cssPlugin::stylesOnlyPluginOptions'
 }
