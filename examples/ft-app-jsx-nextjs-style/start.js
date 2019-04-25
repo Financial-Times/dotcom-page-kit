dd2bfe4ed648b4cc4735a8e69c4660944b12fa10
Matt Hinchliffe <matt@maketea.co.uk>
2019-04-25 08:14:21 +0100

    Remove JSX renderer package as this has been superseded by the React package

--- examples/ft-app-jsx-nextjs-style/start.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
-require('sucrase/register')
-
-const port = process.env.PORT || 3000
-const app = require('./src/server').app
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // eslint-disable-line no-console
