05570f8ba82f85cf8dd15b1f04ca02416df135f3
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-04-29 11:11:46 +0100

    Adds more granular snapshot tests for shell component

--- packages/anvil-ui-ft-shell/src/__test__/components/DocumentHead.test.tsx
+++ packages/anvil-ui-ft-shell/src/__test__/components/DocumentHead.test.tsx
@@ -3,29 +3,12 @@ import renderer from 'react-test-renderer'
 import Subject from '../../components/DocumentHead'
 
 describe('anvil-ui-ft-shell/src/components/DocumentHead', () => {
-  // TODO: better tests
   it('renders the document head', () => {
     const props = {
       description: 'Website description.',
       facebookPage: 'facebook-page-id',
-      jsonLd: [{ '@type': 'article', headline: 'JSON+LD headline' }],
-      openGraph: {
-        og: {
-          title: 'Open Graph title',
-          video: 'https://my.site/video.mp4',
-          'video:type': 'video/mp4',
-          'video:width': 640,
-          'video:height': 360
-        },
-        article: {
-          published_time: '2019-02-27T06:59:35.000Z',
-          modified_time: '2019-02-27T16:01:21.000Z',
-          author: ['Joe Bloggs', 'Jane Doe']
-        }
-      },
       pageTitle: 'Page title',
       siteTitle: 'Website title',
-      stylesheets: [],
       twitterSite: '@twitter_page',
       canonicalURL: 'https://my.site'
     }
---
+++ packages/anvil-ui-ft-shell/src/__test__/components/OpenGraph.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Subject from '../../components/OpenGraph'
+
+describe('anvil-ui-ft-shell/src/components/OpenGraph', () => {
+  it('renders the given Open Graph data to meta tags', () => {
+    const fixture = {
+      og: {
+        title: 'Open Graph title',
+        video: 'https://my.site/video.mp4',
+        'video:type': 'video/mp4',
+        'video:width': 640,
+        'video:height': 360
+      },
+      article: {
+        published_time: '2019-02-27T06:59:35.000Z',
+        modified_time: '2019-02-27T16:01:21.000Z',
+        author: ['Joe Bloggs', 'Jane Doe']
+      }
+    }
+
+    const tree = renderer.create(<Subject openGraph={fixture} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
