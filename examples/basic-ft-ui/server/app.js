f19b0cc37954c502e8f15b3de734bf5542d09f73
Maggie Allen <maggie.allen@ft.com>
2019-05-08 15:37:55 +0100

    remove editions middleware from the basic-ft-ui example app

--- examples/basic-ft-ui/server/app.js
+++ examples/basic-ft-ui/server/app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 
-const editionMiddleware = require('@financial-times/anvil-middleware-ft-edition')
 const navigationMiddleware = require('@financial-times/anvil-middleware-ft-navigation')
 
 const app = express()
 
-app.use(editionMiddleware.init())
 app.use(navigationMiddleware.init())
 
 app.use('/dist', express.static('./dist'))
