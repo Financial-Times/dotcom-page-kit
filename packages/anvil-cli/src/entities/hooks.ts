8ad2f8d364bf1b79b618d5a67ec3106ecbb8da16
Matt Hinchliffe <matt@maketea.co.uk>
2019-05-08 12:20:38 +0100

    Refactor build tool base Babel config to use hooks defined as constants

--- packages/anvil-cli/src/entities/hooks.ts
+++ packages/anvil-cli/src/entities/hooks.ts
@@ -1,4 +1,6 @@
 export enum hooks {
+  BABEL_CONFIG = 'babelConfig',
+  BABEL_PRESET_ENV_OPTIONS = 'babelConfig::preset::env::options',
   CLEAN_WEBPACK_PLUGIN_OPTIONS = 'webpackConfig::plugins::cleanWebpackPlugin::options',
   CLEAN_WEBPACK_PLUGIN_PATHS = 'webpackConfig::plugins::cleanWebpackPlugin::paths',
   ENTRYPOINTS = 'webpackConfig::entry',
--- packages/anvil-cli/src/operations/getBabelConfig.ts
+++ packages/anvil-cli/src/operations/getBabelConfig.ts
@@ -1,4 +1,5 @@
 import get from 'lodash.get'
+import { hooks } from '../entities/hooks'
 import { CliContext } from '../entities/CliContext'
 
 /**
@@ -28,8 +29,8 @@ export function getBabelConfig(cli?: CliContext) {
   }
 
   if (cli) {
-    cli.publish('babelConfig', babelConfig)
-    cli.publish('babelConfig::preset::env::options', presetEnvOpts)
+    cli.publish(hooks.BABEL_CONFIG, babelConfig)
+    cli.publish(hooks.BABEL_PRESET_ENV_OPTIONS, presetEnvOpts)
   }
 
   return babelConfig
