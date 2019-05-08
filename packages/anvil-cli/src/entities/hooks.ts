334af5af8ff9999a74f611c222f8ddbab84aa8fa
Matt Hinchliffe <matt@maketea.co.uk>
2019-05-08 12:15:19 +0100

    Refactor build tool base Webpack config to use hooks defined as constants

---
+++ packages/anvil-cli/src/entities/hooks.ts
@@ -0,0 +1,8 @@
+export enum hooks {
+  CLEAN_WEBPACK_PLUGIN_OPTIONS = 'webpackConfig::plugins::cleanWebpackPlugin::options',
+  CLEAN_WEBPACK_PLUGIN_PATHS = 'webpackConfig::plugins::cleanWebpackPlugin::paths',
+  ENTRYPOINTS = 'webpackConfig::entry',
+  JS_RULE = 'webpackConfig::jsRule',
+  MANIFEST_PLUGIN_OPTIONS = 'webpackConfig::plugins::manifestPlugin::options',
+  WEBPACK_CONFIG = 'webpackConfig'
+}
