b0054a64d0b071e65e45736e4cfa463d5550004d
Maggie Allen <maggie.allen@ft.com>
2019-05-10 12:12:40 +0100

    assign editions as a navigation property

--- examples/basic-ft-ui/server/controllers/home.js
+++ examples/basic-ft-ui/server/controllers/home.js
@@ -2,7 +2,7 @@ const ReactDOM = require('react-dom/server')
 const Document = require('../components/Document')
 
 module.exports = (_, response, next) => {
-  const navigationData = { ...response.locals.navigation, editions: response.locals.editions }
+  const navigationData = { ...response.locals.navigation }
 
   try {
     const page = Document({ navigationData })
