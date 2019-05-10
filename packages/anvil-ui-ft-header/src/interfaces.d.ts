b2cb6264a8181fa4dd870e2e07bb6b530b6b9ce2
Matt Hinchliffe <matt@maketea.co.uk>
2019-05-10 15:03:08 +0100

    Clean up old header component types

--- packages/anvil-ui-ft-header/src/interfaces.d.ts
+++ packages/anvil-ui-ft-header/src/interfaces.d.ts
@@ -18,32 +18,6 @@ export interface THeaderProps {
 
 export type THeaderVariant = 'simple' | 'home' | 'sticky' | 'logo-only'
 
-// export interface TItemSubMenu {
-//   label: string | null
-//   items: TItem[]
-// }
-// export interface TItem {
-//   label: string | null
-//   url: string | null
-//   submenu?: TItemSubMenu | null
-//   selected?: boolean
-// }
-
-// export type TUserMenu = {
-//   label: string
-//   items: TItem[]
-// }
-
-// export type NavListProps = {
-//   navbarOptions: any[]
-//   variant?: string
-// }
-
-// export interface IDrawerParent {
-//   props: TItem
-//   index?
-// }
-
 export type TEditions = {
   current: { name: string }
   others: TEdition[]
