e15fca168b0971169bfdf0d570338bd5a8d6cf1c
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-10 14:00:22 +0100

    Use constants for hooks in the `esnext` plugin

---
+++ packages/anvil-plugin-esnext/src/hooks.ts
@@ -0,0 +1,5 @@
+export enum hooks {
+  BABEL_CLASS_PROPERTIES_PLUGIN_OPTIONS = 'babelConfig::plugin::proposalClassProperties::options',
+  BABEL_TRANSFORM_RUNTIME_PLUGIN_OPTIONS = 'babelConfig::plugin::transformRuntime::options',
+  BABEL_SYNTAX_DYNAMIC_IMPORT_PLUGIN_OPTIONS = 'babelConfig::plugin::syntaxDynamicImport::options'
+}
