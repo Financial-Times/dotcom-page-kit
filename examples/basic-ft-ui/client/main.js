2e7a83715c8e104a6c9e92bb7e23e4a285e15175
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-15 14:28:12 +0100

    Initialise tracking in the `basic-ft-ui` example

--- examples/basic-ft-ui/client/main.js
+++ examples/basic-ft-ui/client/main.js
@@ -1,6 +1,8 @@
+import tracking from '@financial-times/n-tracking'
 import domLoaded from 'dom-loaded'
 import * as layout from '@financial-times/anvil-ui-ft-layout'
 
 domLoaded.then(() => {
   layout.init()
+  tracking.init()
 })
