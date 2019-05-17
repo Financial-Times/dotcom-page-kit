78272feaa54cf3cbb3e1bea3985ca87f60de26eb
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-17 12:30:33 +0100

    Remove support for babel presets

--- packages/anvil-plugin-esnext/src/babel.ts
+++ packages/anvil-plugin-esnext/src/babel.ts
@@ -1,15 +1,7 @@
 import { hooks } from './hooks'
 import { CliContext } from '@financial-times/anvil-cli'
 
-/**
- * Returns the babel config.
- *
- * NOTE: This file can also be specified as a preset in a .babelrc file.
- * When used in such a manner, there will be no args supplied to the function,
- * hence why the arg is optional.
- */
-
-export default (cli?: CliContext) => {
+export default (cli: CliContext) => {
   const classPropertiesPluginOptions = {}
   const syntaxDynamicImportPluginOptions = {}
   const transformRuntimePluginOptions = {}
@@ -23,11 +15,9 @@ export default (cli?: CliContext) => {
     ]
   }
 
-  if (cli) {
-    cli.publish(hooks.BABEL_CLASS_PROPERTIES_PLUGIN_OPTIONS, classPropertiesPluginOptions)
-    cli.publish(hooks.BABEL_SYNTAX_DYNAMIC_IMPORT_PLUGIN_OPTIONS, syntaxDynamicImportPluginOptions)
-    cli.publish(hooks.BABEL_TRANSFORM_RUNTIME_PLUGIN_OPTIONS, transformRuntimePluginOptions)
-  }
+  cli.publish(hooks.BABEL_CLASS_PROPERTIES_PLUGIN_OPTIONS, classPropertiesPluginOptions)
+  cli.publish(hooks.BABEL_SYNTAX_DYNAMIC_IMPORT_PLUGIN_OPTIONS, syntaxDynamicImportPluginOptions)
+  cli.publish(hooks.BABEL_TRANSFORM_RUNTIME_PLUGIN_OPTIONS, transformRuntimePluginOptions)
 
   return config
 }
