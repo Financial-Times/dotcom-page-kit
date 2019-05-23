caae65375f51ca1642564ca0971d2e6c709b9377
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-23 11:17:26 +0100

    Implement the `<AppContext />` component of the `anvil-ft-app-context` package

---
+++ packages/anvil-ft-app-context/src/client/__test__/fixtures.ts
@@ -0,0 +1,10 @@
+export const appContext = {
+  app: 'article',
+  edition: 'uk',
+  version: '882797258625531f20d604f6441ef8cfcb2d772b',
+  abState: 'subscriberCohort:on,premiumCohort:on,nonUSACohort:on',
+  contentId: 'c5935758-7730-11e9-bbad-7c18c0ea0201',
+  contentType: 'article',
+  isProduction: true,
+  publishReference: 'tid_17wmwszvk3'
+}
