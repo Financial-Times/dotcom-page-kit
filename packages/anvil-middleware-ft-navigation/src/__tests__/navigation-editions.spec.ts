c800b3c303bbffc5056ebf3382dcc235c0f15a1e
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-05-23 13:48:19 +0100

    Add comment and spec for editions header behaviour, fixes #343

--- packages/anvil-middleware-ft-navigation/src/__tests__/navigation-editions.spec.ts
+++ packages/anvil-middleware-ft-navigation/src/__tests__/navigation-editions.spec.ts
@@ -1,7 +1,7 @@
 import { navigationEditions as subject } from '../navigation-editions'
 import httpMocks from 'node-mocks-http'
 
-const editionsUk = {
+const editionsUK = {
   current: expect.objectContaining({ id: 'uk' }),
   others: [expect.objectContaining({ id: 'international' })]
 }
@@ -16,29 +16,58 @@ let requestMock
 let requestMockWithQueryString
 let responseMock
 
-beforeEach(() => {
-  responseMock = httpMocks.createResponse()
-  requestMock = httpMocks.createRequest()
-  requestMockWithQueryString = httpMocks.createRequest({ query: { edition: 'international' } })
-  instance = subject(requestMock, responseMock)
-})
-
-afterEach(() => {
-  instance = null
-})
-
-it('returns uk as the current edition by default', () => {
-  expect(instance).toEqual(editionsUk)
-})
-
-describe('when international is passed as a query string parameter', () => {
-  it('returns international edition', () => {
-    instance = subject(requestMockWithQueryString, responseMock)
-    expect(instance).toEqual(editionsInternational)
+describe('anvil-middleware-ft-navigation/src/navigation-editions', () => {
+  beforeEach(() => {
+    responseMock = httpMocks.createResponse()
+    requestMock = httpMocks.createRequest()
+    requestMockWithQueryString = httpMocks.createRequest({ query: { edition: 'international' } })
+    instance = subject(requestMock, responseMock)
   })
 
-  it('sets a next-edition cookie', () => {
-    instance = subject(requestMockWithQueryString, responseMock)
-    expect(responseMock.cookies).toHaveProperty('next-edition')
+  afterEach(() => {
+    instance = null
+  })
+
+  describe('with no query string parameter or cookie set', () => {
+    it('returns UK edition by default', () => {
+      expect(instance).toEqual(editionsUK)
+    })
+  })
+
+  describe('with header set', () => {
+    let request
+    let response
+    let result
+
+    beforeEach(() => {
+      request = httpMocks.createRequest({ headers: { 'ft-edition': 'international' } })
+      response = httpMocks.createResponse()
+      result = subject(request, response)
+    })
+
+    it('returns the set edition', () => {
+      expect(result).toEqual(editionsInternational)
+    })
+  })
+
+  describe('with a query string parameter set', () => {
+    let request
+    let response
+    let result
+
+    beforeEach(() => {
+      request = httpMocks.createRequest({ query: { edition: 'international' } })
+      response = httpMocks.createResponse()
+      result = subject(request, response)
+    })
+
+    it('returns the set edition', () => {
+      expect(result).toEqual(editionsInternational)
+    })
+
+    it('sets a next-edition cookie', () => {
+      instance = subject(requestMockWithQueryString, responseMock)
+      expect(response.cookies).toHaveProperty('next-edition')
+    })
   })
 })
