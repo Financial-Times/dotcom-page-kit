c5509ac3824b250d988ee99f6d6c546e6a556288
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-23 15:03:54 +0100

    Implement the server side app context client of the `anvil-ft-app-context` package

---
+++ packages/anvil-ft-app-context/src/server/AppContext.ts
@@ -0,0 +1,63 @@
+import dashify from 'dashify'
+import camelCase from 'camelcase'
+import pascalCase from 'pascalcase'
+import { prepareEmbedString } from '../helpers/prepareEmbedString'
+import { TAppContext, TLegacyAppContextDataAttributes } from '../types'
+
+const legacyKeyMap = {
+  app: 'data-next-app',
+  edition: 'data-next-edition',
+  version: 'data-next-version',
+  isProduction: 'data-next-is-production'
+}
+
+export interface AppContextConstructorProps {
+  context?: Partial<TAppContext>
+}
+
+export class AppContext {
+  data: Partial<TAppContext>
+
+  constructor({ context = {} }: AppContextConstructorProps = {}) {
+    this.data = context
+  }
+
+  add(additionalContext: Partial<TAppContext>) {
+    this.data = { ...this.data, ...additionalContext }
+  }
+
+  toEmbedString(): string {
+    return prepareEmbedString(this.data as TAppContext)
+  }
+
+  toLegacyDataAttributesString(): string {
+    const attributes = []
+
+    attributes.push('data-app-context')
+
+    for (let key of Object.keys(this.data).sort()) {
+      const attrName = legacyKeyMap[key] ? legacyKeyMap[key] : `data-${dashify(key)}`
+
+      if (typeof this.data[key] === 'boolean') {
+        if (this.data[key]) {
+          attributes.push(attrName)
+        }
+      } else {
+        attributes.push(`${attrName}="${this.data[key]}"`)
+      }
+    }
+
+    return attributes.join(' ')
+  }
+
+  toLegacyDataAttributesObject(): TLegacyAppContextDataAttributes {
+    const attributes = { dataAppContext: true } as any
+
+    for (let key of Object.keys(this.data).sort()) {
+      const prop = legacyKeyMap[key] ? camelCase(legacyKeyMap[key]) : `data${pascalCase(key)}`
+      attributes[prop] = this.data[key]
+    }
+
+    return attributes
+  }
+}
---
+++ packages/anvil-ft-app-context/src/server/__test__/AppContext.spec.ts
@@ -0,0 +1,90 @@
+import { AppContext } from '../AppContext'
+import { prepareEmbedString } from '../../helpers/prepareEmbedString'
+import { appContextWithExtras as context } from '../../client/__test__/fixtures'
+
+describe('AppContext', () => {
+  describe('.data', () => {
+    it('returns the app context data', () => {
+      const appContext = new AppContext({ context })
+      expect(appContext.data).toEqual(context)
+    })
+  })
+
+  describe('.toEmbedString()', () => {
+    it('returns the script embed string', () => {
+      const appContext = new AppContext({ context })
+      const embedString = prepareEmbedString(context)
+      const result = appContext.toEmbedString()
+      expect(result).toBe(embedString)
+    })
+  })
+
+  describe('.toLegacyDataAttributesString()', () => {
+    it('returns a data attributes string that contains the legacy attribute names', () => {
+      const appContext = new AppContext({ context })
+      const legacyAttributesString =
+        'data-app-context ' +
+        `data-ab-state="${context.abState}" ` +
+        `data-next-app="${context.app}" ` +
+        `data-bar-prop="${context.barProp}" ` +
+        `data-content-id="${context.contentId}" ` +
+        `data-content-type="${context.contentType}" ` +
+        `data-next-edition="${context.edition}" ` +
+        `data-foo-prop="${context.fooProp}" ` +
+        'data-next-is-production ' +
+        `data-publish-reference="${context.publishReference}" ` +
+        `data-next-version="${context.version}"`
+      const result = appContext.toLegacyDataAttributesString()
+      expect(result).toBe(legacyAttributesString)
+    })
+
+    it('ensures that attributes with false values are not outputted', () => {
+      const appContext = new AppContext({ context: { ...context, fooFalse: false, fooTrue: true } })
+      const legacyAttributesString =
+        'data-app-context ' +
+        `data-ab-state="${context.abState}" ` +
+        `data-next-app="${context.app}" ` +
+        `data-bar-prop="${context.barProp}" ` +
+        `data-content-id="${context.contentId}" ` +
+        `data-content-type="${context.contentType}" ` +
+        `data-next-edition="${context.edition}" ` +
+        `data-foo-prop="${context.fooProp}" ` +
+        'data-foo-true ' +
+        'data-next-is-production ' +
+        `data-publish-reference="${context.publishReference}" ` +
+        `data-next-version="${context.version}"`
+      const result = appContext.toLegacyDataAttributesString()
+      expect(result).toBe(legacyAttributesString)
+    })
+  })
+
+  describe('.toLegacyDataAttributesObject()', () => {
+    it('returns a data attributes object that contains the legacy attribute names', () => {
+      const appContext = new AppContext({ context })
+      const legacyAttributesObject = {
+        dataAppContext: true,
+        dataAbState: context.abState,
+        dataNextApp: context.app,
+        dataBarProp: context.barProp,
+        dataFooProp: context.fooProp,
+        dataContentId: context.contentId,
+        dataContentType: context.contentType,
+        dataNextEdition: context.edition,
+        dataNextVersion: context.version,
+        dataNextIsProduction: true,
+        dataPublishReference: context.publishReference
+      }
+      const result = appContext.toLegacyDataAttributesObject()
+      expect(result).toEqual(legacyAttributesObject)
+    })
+  })
+
+  describe('.add({...})', () => {
+    it('adds properties unto the app context', () => {
+      const appContext = new AppContext({ context })
+      const additionalContext = { foo: 'one', bar: 'two', contentId: '12345' }
+      appContext.add(additionalContext)
+      expect(appContext.data).toEqual({ ...context, ...additionalContext })
+    })
+  })
+})
