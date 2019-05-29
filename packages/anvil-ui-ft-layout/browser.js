37742e6be1f20f0a5f860dc1744283b22664bfee
Maggie Allen <maggie.allen@ft.com>
2019-05-29 15:11:40 +0100

    define rootElement in browser.js

--- packages/anvil-ui-ft-layout/browser.js
+++ packages/anvil-ui-ft-layout/browser.js
@@ -3,6 +3,7 @@ import * as header from '@financial-times/anvil-ui-ft-header/browser'
 import oTypography from 'o-typography'
 
 export function init({ headerOptions = {}, footerOptions = {} } = {}) {
+  const rootElement = document.querySelector('.n-layout')
   oTypography.init(rootElement)
   header.init(headerOptions)
   footer.init(footerOptions)
