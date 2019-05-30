20e19157d9570deb68061c8542ba34a0d539edbf
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-05-30 18:21:46 +0100

    Refactor express jsx example home controller to match express handlebars example

--- examples/express-with-jsx/server/controllers/home.js
+++ examples/express-with-jsx/server/controllers/home.js
@@ -1,3 +1,14 @@
-export function homePageController(req, res) {
-  res.render('Home.jsx', { greeting: 'hello world...' })
+import catData from '../lib/data.json'
+
+export function homePageController(request, response, next) {
+  const renderOptions = {
+    pageTitle: 'Welcome',
+    catData
+  }
+
+  try {
+    response.render('Home.jsx', renderOptions)
+  } catch (error) {
+    next(error)
+  }
 }
