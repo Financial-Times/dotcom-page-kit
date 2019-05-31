12dcd06b5fdf507fb0cb6a8b32c9284edf9045b5
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-31 13:57:46 +0100

    Update the kitchen sink example to feature app context usage

--- examples/kitchen-sink/server/controllers/home.js
+++ examples/kitchen-sink/server/controllers/home.js
@@ -2,11 +2,13 @@ const React = require('react')
 const ReactDOM = require('react-dom/server')
 const { Shell } = require('@financial-times/anvil-ui-ft-shell')
 const { Layout } = require('@financial-times/anvil-ui-ft-layout')
+const { AppContextEmbed } = require('@financial-times/anvil-ft-app-context')
 const polyfills = require('@financial-times/anvil-ui-ft-polyfills')
 
 module.exports = (_, response, next) => {
   try {
     const flags = { ads: true, tracking: true }
+    const appContext = response.locals.appContext
     const styleBundles = response.locals.assets.loader.getStylesheetURLsFor('styles')
     const scriptBundles = response.locals.assets.loader.getScriptURLsFor('scripts')
     const enhancedScripts = [polyfills.enhanced, ...scriptBundles]
@@ -25,7 +27,10 @@ module.exports = (_, response, next) => {
         pageTitle="Hello World"
         coreScripts={coreScripts}
         stylesheets={styleBundles}
-        enhancedScripts={enhancedScripts}>
+        enhancedScripts={enhancedScripts}
+        htmlAttributes={appContext.toLegacyDataAttributesObject()}>
+        <AppContextEmbed context={appContext.data} />
+
         <Layout navigationData={response.locals.navigation}>
           <div align="center">
             <p className="hello">Hello, welcome to Anvil.</p>
