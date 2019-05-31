ffea1fc7766c0c300373e59daa080cea64993fa7
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-05-31 13:57:46 +0100

    Update the kitchen sink example to feature app context usage

--- examples/kitchen-sink/server/app.js
+++ examples/kitchen-sink/server/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const navigation = require('@financial-times/anvil-middleware-ft-navigation')
+const appContext = require('@financial-times/anvil-middleware-ft-app-context')
 const assets = require('@financial-times/anvil-middleware-assets')
 
 const app = express()
 
 app.use(navigation.init(), assets.init({ hostStaticAssets: true }))
+app.use(appContext.init())
 
 app.get('/', require('./controllers/home'))
 
