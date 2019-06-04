528d9ddf22d632014be0c12a36c265891a481b93
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-06-04 09:33:23 +0100

    Amend the `<Shell/>` component so that it treats boolean properties on the `htmlAttributes` object as boolean attributes

--- packages/anvil-ui-ft-shell/src/__test__/lib/formatAttributeNames.spec.ts
+++ packages/anvil-ui-ft-shell/src/__test__/lib/formatAttributeNames.spec.ts
@@ -15,10 +15,24 @@ describe('anvil-ui-open-graph/src/lib/formatAttributeNames', () => {
 
   it('converts camelCase property names to kebab-case', () => {
     const result = subject(fixture)
-
     expect(result).toHaveProperty('data-version', 123)
     expect(result).toHaveProperty('data-app-name', 'test')
     expect(result).toHaveProperty('user-logged-in', true)
     expect(result).toHaveProperty('lang', 'en-GB')
+  })
+
+  it('converts boolean data properties to boolean data attributes', () => {
+    const fixture = { dataIsProduction: true, dataIsDevelopment: false, userIsLoggedIn: true }
+    const result = subject(fixture)
+
+    // where react is concerned, a `true` boolean data attribute
+    // is one where the attribute value is an empty string (because
+    // it is not possible to render the attribute without a value),
+    // and a `false` boolean data attribute is one where the attribute
+    // has not been specified altogether
+    expect(result).toEqual({
+      'data-is-production': '',
+      'user-is-logged-in': true
+    })
   })
 })
