845c4ec415953c30afe1797e0f39caf359c1ba82
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2019-06-04 09:33:23 +0100

    Amend the `<Shell/>` component so that it treats boolean properties on the `htmlAttributes` object as boolean attributes

--- packages/anvil-ui-ft-shell/src/lib/formatAttributeNames.ts
+++ packages/anvil-ui-ft-shell/src/lib/formatAttributeNames.ts
@@ -6,7 +6,22 @@ export default function formatAttributeNames(data: TAttributeData = {}) {
   const output = {}
 
   for (const [key, value] of Object.entries(data)) {
-    output[hyphenateString(key)] = value
+    const hyphenatedKey = hyphenateString(key)
+
+    // Let's render boolean data attributes properly
+    // as per https://github.com/Financial-Times/anvil/issues/370
+    if (hyphenatedKey.startsWith('data-') && typeof value === 'boolean') {
+      // Where react is concerned, a `true` boolean data attribute
+      // is one where the attribute value is an empty string (because
+      // it is not possible to render an attribute without a value),
+      // and a `false` boolean data attribute is one where the attribute
+      // has not been specified altogether
+      if (value) {
+        output[hyphenatedKey] = ''
+      }
+    } else {
+      output[hyphenatedKey] = value
+    }
   }
 
   return output
