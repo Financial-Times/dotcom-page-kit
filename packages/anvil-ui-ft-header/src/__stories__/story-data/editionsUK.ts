f30665dbe61cf8d9026c22ac6b75c02068c1114e
Maggie Allen <maggie.allen@ft.com>
2019-06-05 13:50:39 +0100

    update editions storybook fixtures

--- packages/anvil-ui-ft-header/src/__stories__/story-data/editionsUK.ts
+++ packages/anvil-ui-ft-header/src/__stories__/story-data/editionsUK.ts
@@ -1,8 +1,8 @@
 import { TNavEditions } from '@financial-times/anvil-types-navigation'
 
 const data: TNavEditions = {
-  current: { id: 'uk', name: 'UK Edition', url: '#' },
-  others: [{ id: 'international', name: 'International Edition', url: '#' }]
+  current: { id: 'uk', name: 'UK', url: '#' },
+  others: [{ id: 'international', name: 'International', url: '#' }]
 }
 
 export default data
