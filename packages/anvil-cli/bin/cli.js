2d8cb61288e3c873eeca60ada8ce048b543de2ba
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-06-07 15:50:23 +0100

    Update package main entry points from cjs/ to node/

--- packages/anvil-cli/bin/cli.js
+++ packages/anvil-cli/bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-// NOTE: `../dist/cjs` will be present in the distributed package, so it's absence should not cause lint checks to fail
-const { executeCli } = require('../dist/cjs') // eslint-disable-line import/no-unresolved
+// NOTE: `../dist/node` will be present in the distributed package, so it's absence should not cause lint checks to fail
+const { executeCli } = require('../dist/node') // eslint-disable-line import/no-unresolved
 
 executeCli({
   argv: process.argv,
--- packages/anvil-server-react/cjs.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./dist/cjs/index').default // eslint-disable-line import/no-unresolved
