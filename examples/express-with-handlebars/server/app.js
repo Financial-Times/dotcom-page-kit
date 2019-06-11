2bc2a2813972930969f4301f0e10752608a1e959
Matt Hinchliffe <matt@maketea.co.uk>
2019-06-11 09:08:34 +0100

    Update example apps to use named exports from React and Handlebars packages

--- examples/express-with-handlebars/server/app.js
+++ examples/express-with-handlebars/server/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const hbs = require('./lib/handlebars')
+const homePageController = require('./controllers/home')
+const { HandlebarsRenderer } = require('@financial-times/anvil-server-handlebars')
 
-const app = express()
+const app = (module.exports = express())
 
 app.locals.siteName = 'Good Dogs'
 
 // Add Handlebars as a view engine so controllers may use response.render()
-app.engine('.hbs', hbs.engine)
+const renderer = new HandlebarsRenderer({
+  cache: process.env.NODE_ENV === 'production'
+})
+
+app.engine('.hbs', renderer.engine)
 
 app.use('/public', express.static('./public'))
 
-app.get('/', require('./controllers/home'))
-
-module.exports = app
+app.get('/', homePageController)
--- examples/express-with-jsx/server/app.js
+++ examples/express-with-jsx/server/app.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import { homePageController } from './controllers/home'
-import AnvilReact from '@financial-times/anvil-server-react'
+import { ReactRenderer } from '@financial-times/anvil-server-react'
 
 export const app = express()
 
 app.locals.siteName = 'Good Cats'
 
 // Add React as a view engine so controllers may use response.render()
-const react = new AnvilReact({ useStaticRendering: true })
-app.engine('.jsx', react.engine)
+const renderer = new ReactRenderer({ useStaticRendering: true })
+app.engine('.jsx', renderer.engine)
 
 app.use('/public', express.static('./public'))
 
