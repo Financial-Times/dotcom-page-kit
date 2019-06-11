f902bd5505cc66a0e813ad9ebeac7cc150654fce
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-06-11 15:11:47 +0100

    Remove JSDOM tests from the kitchen sink integration test

--- examples/kitchen-sink/__test__/integration.test.js
+++ examples/kitchen-sink/__test__/integration.test.js
@@ -1,13 +1,16 @@
 const app = require('../server/app')
 const request = require('supertest')
-const { withEnv, withHtml } = require('@financial-times/anvil-test-utils')
-const { loadDataFromScriptEmbed, loadDataFromAttributesEmbed } = require('@financial-times/anvil-ft-app-context')
 
 describe('examples/express-ft-header', () => {
   let response
 
   beforeEach(async () => {
-    response = await request(app).get('/')
+    response = await request(app)
+      .get('/')
+      // NOTE: FT.com apps expect many headers to be set by the CDN+preflight and/or the router.
+      .set({
+        'ft-edition': 'international'
+      })
   })
 
   it('returns an OK response', () => {
@@ -33,8 +36,6 @@ describe('examples/express-ft-header', () => {
   it('populates drawer elements with navigation data', () => {
     expect(response.text).toContain('o-header__drawer-menu--primary')
     expect(response.text).toContain('o-header__drawer-menu--user')
-    expect(response.text).toContain('o-header__drawer-current-edition')
-    expect(response.text).toContain('data-trackable="edition-switcher"')
   })
 
   it('populates meganav elements with navigation data', () => {
@@ -46,42 +47,16 @@ describe('examples/express-ft-header', () => {
     expect(response.text).toContain('o-header__mega-item')
   })
 
-  describe('app context', () => {
-    const appContext = {
-      product: 'next',
-      edition: 'edition:foo',
-      abTestState: 'state:foo',
-      appVersion: 'foo:version',
-      isProduction: true
-    }
-
-    beforeEach(async () => {
-      await withEnv({
-        env: {
-          NODE_ENV: 'production',
-          SOURCE_VERSION: appContext.appVersion
-        },
-        execute: async () => {
-          response = await request(app)
-            .get('/')
-            .set('ft-ab', appContext.abTestState)
-            .set('ft-edition', appContext.edition)
-        }
-      })
-    })
-
-    it('embeds app context data as legacy data attributes on the html tag', () => {
-      withHtml(response.text, () => {
-        const result = loadDataFromAttributesEmbed()
-        expect(result).toEqual(appContext)
-      })
-    })
-
-    it('embed app context data as a script embed', () => {
-      withHtml(response.text, () => {
-        const result = loadDataFromScriptEmbed()
-        expect(result).toEqual(appContext)
-      })
-    })
+  it('renders edition with current edition selected', () => {
+    expect(response.text).toContain('<p class="o-header__drawer-current-edition">International Edition</p>')
   })
+
+  it('renders app context data as legacy data attributes', () => {
+    // TODO: reimplement this test when app name is being set consistently
+    // expect(response.text).toContain('data-next-app="kitchen-sink"')
+    expect(response.text).toContain('data-next-edition="international"')
+    expect(response.text).toContain('data-next-product="next"')
+  })
+
+  it.skip('renders app context data as embedded JSON', () => {})
 })
