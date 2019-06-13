5067758d4ee7ef916481893db2c4ac8b6697b0d5
Maggie Allen <maggie.allen@ft.com>
2019-06-13 17:22:36 +0100

    rename loadAppContext module

--- packages/anvil-ui-ft-app-context/browser.js
+++ packages/anvil-ui-ft-app-context/browser.js
@@ -1,7 +1,7 @@
 import { AppContextClient } from './src/client/AppContextClient'
-import { loadEmbeddedAppContextData } from './src/client/loadAppContext'
+import { loadAppContext } from './src/client/loadAppContext'
 
 export function init() {
-  const context = loadEmbeddedAppContextData()
+  const context = loadAppContext()
   return new AppContextClient({ context })
 }
