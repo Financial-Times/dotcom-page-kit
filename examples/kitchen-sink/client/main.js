49fb6b7ee55e3388e6bc5d033c8302a8ece77b44
Sami Triki <contact@triki.io>
2019-06-14 11:05:48 +0100

    Integrate n-ads and leaderboard slot

--- examples/kitchen-sink/client/main.js
+++ examples/kitchen-sink/client/main.js
@@ -3,10 +3,20 @@ import domLoaded from 'dom-loaded'
 import * as flags from '@financial-times/anvil-ui-ft-flags'
 import * as layout from '@financial-times/anvil-ui-ft-layout'
 import * as appContext from '@financial-times/anvil-ft-app-context'
+import { init as initAds } from '@financial-times/n-ads'
 
 domLoaded.then(() => {
   const flagsClient = flags.init()
   const appContextClient = appContext.init()
 
   layout.init()
+
+  initAds(
+    {
+      trackingCallback: console.log // eslint-disable-line no-console
+    },
+    flagsClient
+  ).then(() => {
+    // Ads slots are ready and will request ads
+  })
 })
