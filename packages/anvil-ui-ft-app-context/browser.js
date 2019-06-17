47a3af448f143c04f195be019099cc242277d086
Maggie Allen <maggie.allen@ft.com>
2019-06-17 18:08:01 +0100

    move browser.ts inside the src directory

--- packages/anvil-ui-ft-app-context/browser.js
+++ packages/anvil-ui-ft-app-context/browser.js
@@ -1,7 +1,1 @@
-import { AppContext } from './src/client/AppContext'
-import { loadAppContext } from './src/client/loadAppContext'
-
-export function init() {
-  const context = loadAppContext()
-  return new AppContext({ context })
-}
+export * from './dist/browser/client/browser'
