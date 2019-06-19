815bd384e14e7d6bee04f4519941bda611aa1eee
Maggie Allen <maggie.allen@ft.com>
2019-06-19 12:22:40 +0100

    use v4 of the focus-visible polyfill as v5 is not supported by o-normalise

--- packages/anvil-ui-ft-layout/browser.js
+++ packages/anvil-ui-ft-layout/browser.js
@@ -2,6 +2,9 @@ import * as footer from '@financial-times/anvil-ui-ft-footer/browser'
 import * as header from '@financial-times/anvil-ui-ft-header/browser'
 import oTypography from 'o-typography'
 // Polyfill for :focus-visible https://github.com/WICG/focus-visible
+// NOTE: v5 of this polyfill is not yet supported by o-normalise
+// https://github.com/WICG/focus-visible/pull/196/files
+// https://github.com/Financial-Times/o-normalise/issues/41
 import 'focus-visible'
 
 export function init({ headerOptions = {}, footerOptions = {} } = {}) {
