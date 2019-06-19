dcf3ad390a964c25b80120be198322277c87fdd8
Maggie Allen <maggie.allen@ft.com>
2019-06-19 12:16:23 +0100

    move focus-visible polyfill into client

--- packages/anvil-ui-ft-layout/browser.js
+++ packages/anvil-ui-ft-layout/browser.js
@@ -1,6 +1,8 @@
 import * as footer from '@financial-times/anvil-ui-ft-footer/browser'
 import * as header from '@financial-times/anvil-ui-ft-header/browser'
 import oTypography from 'o-typography'
+// Polyfill for :focus-visible https://github.com/WICG/focus-visible
+import 'focus-visible'
 
 export function init({ headerOptions = {}, footerOptions = {} } = {}) {
   const rootElement = document.querySelector('.n-layout')
