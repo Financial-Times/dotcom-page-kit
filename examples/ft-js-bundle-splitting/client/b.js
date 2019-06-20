82cab7b52ababb997643a2ad88e3ee114385c34c
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-06-20 17:21:31 +0100

    Remove FT JS bundle splitting example app

--- examples/ft-js-bundle-splitting/client/b.js
+++
@@ -1,6 +0,0 @@
-import ftdomdelegate from 'ftdomdelegate'
-import oDate from 'o-date'
-import oViewport from 'o-viewport'
-import superstoreSync from 'superstore-sync'
-
-console.log({ ftdomdelegate, oDate, oViewport, superstoreSync }) // eslint-disable-line
