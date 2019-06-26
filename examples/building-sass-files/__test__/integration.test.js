14603365097020ede3a9e00affa32974fd6704d8
Matt Hinchliffe <matt@maketea.co.uk>
2019-06-26 16:36:40 +0100

    Replace Sass example integration snapshot test with more detailed assertions

--- examples/building-sass-files/__test__/integration.test.js
+++ examples/building-sass-files/__test__/integration.test.js
@@ -5,7 +5,26 @@ const outputFile = path.join(__dirname, '../dist/styles.css')
 const outputContents = fs.readFileSync(outputFile).toString()
 
 describe('examples/building-sass-files', () => {
-  it('compiles Sass files to CSS file', () => {
-    expect(outputContents).toMatchSnapshot()
+  it('does not output any JS files', () => {
+    const targetFile = path.join(__dirname, '../dist/styles.js')
+    expect(fs.existsSync(targetFile)).toBe(false)
+  })
+
+  describe('Sass', () => {
+    it('can load partials from packages installed with npm', () => {
+      // Styles should be defined by the o-normalise package
+      expect(outputContents).toContain('.o-normalise-visually-hidden')
+    })
+  })
+
+  describe('PostCSS', () => {
+    it('uses Autoprefixer to apply vendor prefixes', () => {
+      expect(outputContents).toContain('-webkit-hyphens:auto;-ms-hyphens:auto;hyphens:auto')
+    })
+
+    it('uses cssnano to minify the output', () => {
+      const result = outputContents.match(/\.selector/g)
+      expect(result.length).toBe(1)
+    })
   })
 })
