23d07acdb3615c669ea8c37f379a35fff0388ba7
Maggie Allen <maggie.allen@ft.com>
2019-07-26 12:43:22 +0100

    Implement n-tracking in the kitchen-sink app

--- examples/kitchen-sink/client/main.js
+++ examples/kitchen-sink/client/main.js
@@ -1,15 +1,17 @@
 import domLoaded from 'dom-loaded'
-// import tracking from '@financial-times/n-tracking'
 import * as flags from '@financial-times/dotcom-ui-flags'
 import * as layout from '@financial-times/dotcom-ui-layout'
 import * as appContext from '@financial-times/dotcom-ui-app-context'
 import { init as initAds } from '@financial-times/n-ads'
+import * as tracking from 'n-tracking'
 
 domLoaded.then(() => {
   const flagsClient = flags.init()
   const appContextClient = appContext.init()
 
   layout.init()
+
+  tracking.init({ appContext: appContextClient.context })
 
   initAds(
     {
