72b39a9631b3904215318ff11db5ffa6f6a9c8f5
Maggie Allen <maggie.allen@ft.com>
2019-07-29 15:23:47 +0100

    Rename the ReactRenderer component to PageKitReactJSX

--- examples/express-with-jsx/server/app.js
+++ examples/express-with-jsx/server/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { homePageController } from './controllers/home'
-import { ReactRenderer } from '@financial-times/dotcom-server-react-jsx'
+import { PageKitReactJSX } from '@financial-times/dotcom-server-react-jsx'
 
 export const app = express()
 
 app.locals.siteName = 'Good Cats'
 
 // Add React as a view engine so controllers may use response.render()
-const renderer = new ReactRenderer({ useStaticRendering: true })
+const renderer = new PageKitReactJSX({ useStaticRendering: true })
 app.engine('.jsx', renderer.engine)
 
 app.use('/public', express.static('./public'))
