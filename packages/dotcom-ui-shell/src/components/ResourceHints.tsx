ef2a46b29e3a2f2f12c276b479fef5c23769269b
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-08-02 11:46:56 +0100

    Add comments and links with details about each preconnect resource hint in shell component

--- packages/dotcom-ui-shell/src/components/ResourceHints.tsx
+++ packages/dotcom-ui-shell/src/components/ResourceHints.tsx
@@ -8,11 +8,31 @@ export type TResourceHintsProps = {
 const ResourceHints = (props: TResourceHintsProps) => {
   return (
     <React.Fragment>
+      {/*
+        Spoor is the API which receives the tracking events sent by the o-tracking library
+        <https://github.com/Financial-Times/o-tracking>
+      */}
       <link rel="preconnect" href="https://spoor-api.ft.com" />
+      {/*
+        The session API is used to validate users and retrieve information about them
+        <https://github.com/Financial-Times/next-session-client>
+      */}
       <link rel="preconnect" href="https://session-next.ft.com" crossOrigin="use-credentials" />
+      {/*
+        The ads API is used to fetch ad targeting information for the current page
+        <https://github.com/Financial-Times/next-ads-api>
+      */}
       <link rel="preconnect" href="https://ads-api.ft.com" />
+      {/*
+        We use the Polyfill Service to provide browser specific JavaScript polyfills
+        <https://www.npmjs.com/package/@financial-times/dotcom-ui-polyfill-service>
+      */}
       <link rel="preconnect" href="https://polyfill.io" />
-      <link rel="preconnect" href="https://www.googletagservices.com" />
+      {/*
+        The Google Publisher Tag library (GPT) is hosted here which is used to deliver ads
+        <https://github.com/Financial-Times/o-ads/blob/master/src/js/ad-servers/gpt.js>
+      */}
+      <link rel="preconnect" href="https://securepubads.g.doubleclick.net" />
 
       {props.resourceHints.map((resource, i) => {
         const type = getResourceType(resource)
