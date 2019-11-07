8515ec725e4d3c237a20739eaadd0f09d8aca6ab
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-11-07 14:56:55 +0000

    Remove SSR with hydration example as it has now been superseded by separate example apps

--- examples/ssr-with-hydration/libs/ssr/server.js
+++
@@ -1,9 +0,0 @@
-export async function getInitialProps({ getInitialProps }, req) {
-  if (!getInitialProps) return {}
-  return await getInitialProps({ req })
-}
-
-export async function getDependencies({ getDependencies }) {
-  if (!getDependencies) return {}
-  return await getDependencies()
-}
