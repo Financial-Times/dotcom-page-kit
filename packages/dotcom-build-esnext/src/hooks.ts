2c72dae50f947d5fb0ee976aea03ba4ffc9a03e5
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-11-14 15:37:21 +0000

    Remove dotcom-build-esnext package as it is unused

--- packages/dotcom-build-esnext/src/hooks.ts
+++
@@ -1,5 +0,0 @@
-export enum hooks {
-  BABEL_PLUGIN_CLASS_PROPERTIES_OPTIONS = 'babelConfig::plugin::proposalClassProperties::options',
-  BABEL_PLUGIN_TRANSFORM_RUNTIME_OPTIONS = 'babelConfig::plugin::transformRuntime::options',
-  BABEL_PLUGIN_SYNTAX_DYNAMIC_IMPORT_OPTIONS = 'babelConfig::plugin::syntaxDynamicImport::options'
-}
