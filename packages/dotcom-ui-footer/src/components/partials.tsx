b446b5795c40b96b7d3b8c5a7208a40ab81cf180
andygout <andygout@hotmail.co.uk>
2019-11-22 17:48:50 +0000

    Append current year to copyright text

--- packages/dotcom-ui-footer/src/components/partials.tsx
+++ packages/dotcom-ui-footer/src/components/partials.tsx
@@ -93,7 +93,7 @@ const CopyrightNotice = ({ withoutMarketsData = false }) => {
   return (
     <div className="o-footer__copyright" role="contentinfo">
       <small>
-        {`${marketsData} © THE FINANCIAL TIMES LTD. `}
+        {`${marketsData} © THE FINANCIAL TIMES LTD ${new Date().getFullYear()}. `}
         <abbr title="Financial Times" aria-label="F T">
           FT
         </abbr>{' '}
