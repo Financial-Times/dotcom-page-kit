958eef9200b441f75b67fe7c5389cf0aa2c1e177
Matt Hinchliffe <matt@maketea.co.uk>
2019-11-28 09:51:28 +0000

    Refactor layout font loading to keep all font information in the same place

--- packages/dotcom-ui-layout/src/font-loading.ts
+++ packages/dotcom-ui-layout/src/font-loading.ts
@@ -2,7 +2,8 @@
 // presence of the cookie does not necessarily mean the file is still
 // available in the browser's cache.
 // NOTE: This function must be written as ES5 code because it will be stringified
-// and embedded in the page to be executed in the browser.
+// and embedded in the page to be executed in the browser. This also means all
+// data and variables must be scoped within the function.
 function loadCustomFonts() {
   var rootElement = document.querySelector('.n-layout')
 
@@ -14,6 +15,11 @@ function loadCustomFonts() {
   }
 }
 
-const loadCustomFontsJS = `(${loadCustomFonts.toString()}());`
+export const loadCustomFontsJS = `(${loadCustomFonts.toString()}());`
 
-export { loadCustomFontsJS }
+export const fontLoadingClassNames = [
+  'o-typography--loading-sans',
+  'o-typography--loading-sansBold',
+  'o-typography--loading-display',
+  'o-typography--loading-displayBold'
+]
