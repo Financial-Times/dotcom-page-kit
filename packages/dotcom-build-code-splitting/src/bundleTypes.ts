b041eb2ca8ed590d1b0b0bb30ad8dab2ce8093d4
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-12-02 16:52:17 +0000

    Integrate disable-tree-shaking-for-chunk-plugin into the code splitting package
    This adds a plugin for each cache group configured to split individual modules. It works by
    iterating over each module in the generated chunk and disables the tracking of `usedExports`.
    This means Webpack will create an export for each named export and maintain the original name.
    
    We want to do this because each app has its own build and deploy step but we want them to reuse
    as much code as possible to prevent our users downloading repeated code as they browse FT.com.
    
    Fixes #677

--- packages/dotcom-build-code-splitting/src/bundleTypes.ts
+++ packages/dotcom-build-code-splitting/src/bundleTypes.ts
@@ -1,5 +1,6 @@
 import memoize from 'memoize-one'
 import getPackageName from 'get-package-name'
+import DisableTreeShakingForChunk from 'disable-tree-shaking-for-chunk-plugin'
 
 // Memoize these calls as modules often need to be resolved many times.
 const extractPackageName = memoize((modulePath: string) => {
@@ -7,13 +8,13 @@ const extractPackageName = memoize((modulePath: string) => {
   return type ? getPackageName(modulePath, type[type.length - 1]) : null
 })
 
-const createSafeFileName = (moduleName: string) => {
+const createChunkName = (moduleName: string) => {
   // Remove or replace any non-safe filename characters
   return moduleName.replace('@', '').replace('/', '-')
 }
 
 /**
- * Create a single bundle which includes all packages in the given list
+ * Create a chunk which includes all packages in the given list of names
  */
 export function createBundleWithPackages(name: string, packageNames: string[]) {
   return {
@@ -35,7 +36,7 @@ export function createBundleWithPackages(name: string, packageNames: string[]) {
 }
 
 /**
- * Create a single bundle which includes all packages which match the given pattern
+ * Create a chunk which includes all modules which match the given pattern
  */
 export function createBundleWithRegExp(name: string, pattern: RegExp) {
   return {
@@ -56,19 +57,33 @@ export function createBundleWithRegExp(name: string, pattern: RegExp) {
 }
 
 /**
- * Create individual bundles for each package in the given list
+ * Create a chunk for each package in the given list of names
  */
 export function createBundlesForPackages(group: string, packageNames: string[]) {
+  // Because we want these individual chunks to be reused between apps as much
+  // as possible we're disabling tree shaking to maximise the chance of apps
+  // generating identical files and content hashes.
+  const disableTreeShakingPlugin = new DisableTreeShakingForChunk({
+    test: new Set()
+  })
+
   return {
+    plugins: [disableTreeShakingPlugin],
     optimization: {
       splitChunks: {
         cacheGroups: {
           [group]: {
-            name: (module) => {
+            name(module) {
               const packageName = extractPackageName(module.context)
-              return createSafeFileName(packageName)
+              const chunkName = createChunkName(packageName)
+
+              // Because we're generating chunk names dynamically we need to append
+              // each name to the list of names used by the disable tree-shaking plugin.
+              disableTreeShakingPlugin.test.add(chunkName)
+
+              return chunkName
             },
-            test: (module) => {
+            test(module) {
               const packageName = extractPackageName(module.context)
               return packageName ? packageNames.includes(packageName) : false
             },
@@ -81,19 +96,33 @@ export function createBundlesForPackages(group: string, packageNames: string[]) 
 }
 
 /**
- * Create individual bundles for each package which matches the given pattern
+ * Create a chunk for each group of modules which match the given pattern
  */
 export function createBundlesForRegExp(group: string, pattern: RegExp) {
+  // Because we want these individual chunks to be reused between apps as much
+  // as possible we're disabling tree shaking to maximise the chance of apps
+  // generating identical files and content hashes.
+  const disableTreeShakingPlugin = new DisableTreeShakingForChunk({
+    test: new Set()
+  })
+
   return {
+    plugins: [disableTreeShakingPlugin],
     optimization: {
       splitChunks: {
         cacheGroups: {
           [group]: {
-            name: (module) => {
+            name(module) {
               const packageName = extractPackageName(module.context)
-              return createSafeFileName(packageName)
+              const chunkName = createChunkName(packageName)
+
+              // Because we're generating chunk names dynamically we need to append
+              // each name to the list of names used by the disable tree-shaking plugin.
+              disableTreeShakingPlugin.test.add(chunkName)
+
+              return chunkName
             },
-            test: (module) => {
+            test(module) {
               return pattern.test(module.context)
             },
             enforce: true
