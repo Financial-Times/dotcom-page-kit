f04013f9e7f331907472fc3281a056d7c56f6e1e
Matt Hinchliffe <matt@maketea.co.uk>
2020-01-03 13:46:44 +0000

    Remove individual Enzyme setup files and configuration from UI packages

--- jest.enzyme.ts
+++ jest.enzyme.ts
@@ -1,3 +1,7 @@
+// This file only configures Enzyme.
+// To use Enzyme in your tests you must:
+// - Configure a JSDOM environment with @jest-env
+// - For Enzyme specific matchers and assertions @import the jest-enzyme package.
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
