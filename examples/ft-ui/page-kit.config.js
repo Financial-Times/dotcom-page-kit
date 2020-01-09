890ebd4b2b0899893ee95d10bad4e2c7ed15c813
Matt Hinchliffe <matt@maketea.co.uk>
2020-01-09 10:58:10 +0000

    Enable webpackImporter for example app Sass so nested dependencies may be resolved

--- examples/ft-ui/page-kit.config.js
+++ examples/ft-ui/page-kit.config.js
@@ -6,8 +6,17 @@ const js = require('@financial-times/dotcom-build-js')
 module.exports = {
   plugins: [
     bower.plugin(),
-    sass.plugin({ includePaths: [path.resolve('../../bower_components')] }),
-    js.plugin(),
+    sass.plugin({
+      // Enabling webpackImporter because Sass itself can only resolve partial files based on the
+      // CWD and not relative to the current file being processed. This means Sass can't find the
+      // nested dependencies created when symlinking.
+      webpackImporter: true,
+      includePaths: [
+        // All Bower components are installed at the repo root
+        path.resolve('../../bower_components')
+      ]
+    }),
+    js.plugin()
   ],
   settings: {
     build: {
