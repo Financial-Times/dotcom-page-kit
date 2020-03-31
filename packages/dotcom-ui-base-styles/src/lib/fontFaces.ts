400f4f3ff391560bcc860aeb97e196b05b99b9d5
Matt Hinchliffe <matt.hinchliffe@ft.com>
2020-03-31 15:37:20 +0100

    Define `$o-fonts-path` to ensure all font URLs are in one place and can be kept in-sync

--- packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
+++ packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
@@ -1,3 +1,5 @@
+// NOTE: please also update ./styles.css when updating the URLs defined below. They
+// need to stay in-sync to ensure our CSS output matches the resource hints we set.
 export const fontFaceURLs = [
   'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.4.1/MetricWeb-Regular.woff',
   'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.4.1/MetricWeb-Semibold.woff',
