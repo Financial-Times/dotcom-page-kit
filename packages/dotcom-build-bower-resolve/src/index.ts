b29e61d9ab983d0a6d5d22ff8985276e0d215198
bren <bren@153.io>
2020-04-01 11:25:26 +0100

    don't overwrite options.resolve in bower plugin

--- packages/dotcom-build-bower-resolve/src/index.ts
+++ packages/dotcom-build-bower-resolve/src/index.ts
@@ -4,6 +4,7 @@ import type webpack from 'webpack'
 export class PageKitBowerResolvePlugin {
   apply(compiler: webpack.Compiler) {
     compiler.options.resolve = {
+      ...compiler.options.resolve,
       // This will find .js files if a bower.json specifies a list of "main" files
       plugins: [new BowerResolvePlugin()],
       // In which folders the resolver look for modules relative paths are
