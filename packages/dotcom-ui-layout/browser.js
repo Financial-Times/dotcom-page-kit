3121d714b73c72bc2600b5a2c7ae2ba831f7cc83
bren <bren@153.io>
2020-04-24 09:16:04 +0100

    use basestyles init from layout

--- packages/dotcom-ui-layout/browser.js
+++ packages/dotcom-ui-layout/browser.js
@@ -1,6 +1,6 @@
 import * as footer from '@financial-times/dotcom-ui-footer/browser'
 import * as header from '@financial-times/dotcom-ui-header/browser'
-import oTypography from 'o-typography'
+import * as baseStyes from '@financial-times/dotcom-ui-base-styles'
 // Polyfill for :focus-visible https://github.com/WICG/focus-visible
 // NOTE: v5 of this polyfill is not yet supported by o-normalise
 // https://github.com/WICG/focus-visible/pull/196/files
@@ -8,7 +8,7 @@ import oTypography from 'o-typography'
 import 'focus-visible'
 
 export function init({ headerOptions = {}, footerOptions = {} } = {}) {
-  oTypography.init()
+  baseStyles.init()
   header.init(headerOptions)
   footer.init(footerOptions)
 }
