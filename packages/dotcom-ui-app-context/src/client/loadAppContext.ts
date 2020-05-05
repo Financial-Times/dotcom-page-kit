bf1b5f7fff581d8e2eace4bb7964217bf19ed755
Nick Colley <nick.colley@ft.com>
2020-05-05 12:22:46 +0100

    Refactor loadAppContext to use loadDataEmbed

--- packages/dotcom-ui-app-context/src/client/loadAppContext.ts
+++ packages/dotcom-ui-app-context/src/client/loadAppContext.ts
@@ -1,18 +1,7 @@
+import { loadDataEmbed } from '@financial-times/dotcom-ui-data-embed'
 import { TAppContext } from '../types'
 import { APP_CONTEXT_ELEMENT_ID } from '../constants'
 
 export default function loadEmbeddedAppContext(): TAppContext {
-  const elem = document.getElementById(APP_CONTEXT_ELEMENT_ID)
-
-  let data = {}
-
-  if (elem) {
-    try {
-      data = JSON.parse(elem.innerHTML)
-    } catch (error) {
-      console.error('Embedded app context could not be loaded:', error) // eslint-disable-line no-console
-    }
-  }
-
-  return Object.freeze(data) as TAppContext
+  return loadDataEmbed(APP_CONTEXT_ELEMENT_ID) as TAppContext
 }
