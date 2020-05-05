2f26db75712b3fae02df9c4fef0c1878e4d8c4d7
Nick Colley <nick.colley@ft.com>
2020-05-05 12:23:19 +0100

    Refactor loadFlags to use loadClientEmbed

--- packages/dotcom-ui-flags/src/client/loadFlags.ts
+++ packages/dotcom-ui-flags/src/client/loadFlags.ts
@@ -1,18 +1,7 @@
+import { loadClientEmbed } from '@financial-times/dotcom-ui-client-embed'
 import { TFlagsData } from '../types'
 import { SCRIPT_ELEMENT_ID } from '../constants'
 
 export default function loadFlags(): TFlagsData {
-  const flagsConfigEl = document.getElementById(SCRIPT_ELEMENT_ID)
-
-  let data = {}
-
-  if (flagsConfigEl) {
-    try {
-      data = JSON.parse(flagsConfigEl.innerHTML)
-    } catch (error) {
-      console.error('Flags configuration error', error) // eslint-disable-line no-console
-    }
-  }
-
-  return Object.freeze(data)
+  return loadClientEmbed(SCRIPT_ELEMENT_ID)
 }
