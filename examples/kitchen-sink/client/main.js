833a000d4ddc673b33f805ed447b9953ee01ab0e
Nick Colley <nick.colley@ft.com>
2020-05-12 12:16:54 +0100

    Add kitchen sink example for dotcom-ui-data-embed package

--- examples/kitchen-sink/client/main.js
+++ examples/kitchen-sink/client/main.js
@@ -2,12 +2,18 @@ import readyState from 'ready-state'
 import * as flags from '@financial-times/dotcom-ui-flags'
 import * as layout from '@financial-times/dotcom-ui-layout'
 import * as appContext from '@financial-times/dotcom-ui-app-context'
+import * as dataEmbed from '@financial-times/dotcom-ui-data-embed'
 import * as tracking from '@financial-times/n-tracking'
 import * as ads from '@financial-times/n-ads'
+
+import { DATA_EMBED_ID } from '../constants.js'
 
 readyState.domready.then(() => {
   const flagsClient = flags.init()
   const appContextClient = appContext.init()
+  const dataEmbedClient = dataEmbed.init({ id: DATA_EMBED_ID })
+
+  console.log('Shared data', dataEmbedClient.getAll()) // eslint-disable-line no-console
 
   layout.init()
 
---
+++ examples/kitchen-sink/constants.js
@@ -0,0 +1,1 @@
+export const DATA_EMBED_ID = 'data-embed'
