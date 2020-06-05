97625f569543841b5bd0a01abe80d4748db66be0
Jake Champion <me@jakechampion.name>
2020-06-05 15:43:40 +0100

    Allow format for icon url to be overridden in imageServiceIconURL

--- packages/dotcom-ui-shell/src/lib/imageServiceIconURL.ts
+++ packages/dotcom-ui-shell/src/lib/imageServiceIconURL.ts
@@ -1,13 +1,13 @@
 import querystring from 'querystring'
 
-function imageServiceIconURL(image: string, size: number): string {
+function imageServiceIconURL(image: string, size: number, format: string): string {
   const serviceURL = 'https://www.ft.com/__origami/service/image/v2/images/raw/'
 
   const queryString = querystring.stringify({
     source: 'update-logos',
     width: size,
     height: size,
-    format: 'png'
+    format: format || 'png'
   })
 
   return `${serviceURL}${encodeURIComponent(image)}?${queryString}`
