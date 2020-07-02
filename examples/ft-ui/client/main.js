996fd6edc39672d596ae2be290ddd10f407e6a7a
Carles Andres <carlos.andres.c@gmail.com>
2020-07-02 13:59:40 +0100

    Added clarification to ft-ui example code

--- examples/ft-ui/client/main.js
+++ examples/ft-ui/client/main.js
@@ -8,6 +8,9 @@ import {
 readyState.domready.then(() => {
   layout.init()
 
+  // These methods won't perform any changes to the footer unless the user is
+  // deemed to be in California.
+  // See this example's README for details on how to fake that.
   addDNSLinkToFooter()
   adaptPrivacyLinkToLegislation()
 })
