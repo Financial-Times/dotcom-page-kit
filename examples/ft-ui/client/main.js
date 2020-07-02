7310717e2608230c44bf78f74d2c2ed8760ed60f
Carles Andres <carlos.andres.c@gmail.com>
2020-07-02 13:41:32 +0100

    Added privacy-footer-localiser to ft-ui example

--- examples/ft-ui/client/main.js
+++ examples/ft-ui/client/main.js
@@ -1,6 +1,13 @@
 import readyState from 'ready-state'
 import * as layout from '@financial-times/dotcom-ui-layout'
+import {
+  addDNSLinkToFooter,
+  adaptPrivacyLinkToLegislation
+} from '@financial-times/dotcom-privacy-footer-localiser'
 
 readyState.domready.then(() => {
   layout.init()
+
+  addDNSLinkToFooter()
+  adaptPrivacyLinkToLegislation()
 })
