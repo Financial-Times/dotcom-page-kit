86921b15e8975b1ead9e2eddcd31ad7e34dfb1b9
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2020-08-11 18:29:50 +0100

    chore(code-splitting): fix tests

--- packages/dotcom-build-code-splitting/src/__tests__/index.spec.ts
+++ packages/dotcom-build-code-splitting/src/__tests__/index.spec.ts
@@ -3,31 +3,35 @@ import webpack from 'webpack'
 import path from 'path'
 
 describe('Code Splitting', () => {
-  it('chunk privacy modules', (done) => {
-    webpack(
-      {
-        entry: {
-          scripts: path.join(__dirname, '__fixtures__', 'entry-point.js')
-        },
-        output: {
-          filename: '[name].js',
-          path: path.join(__dirname, '/tmp')
-        },
-        plugins: [new PageKitCodeSplittingPlugin()]
-      },
-      function (err, stats) {
-        if (err) {
-          return done(err)
-        } else if (stats.hasErrors()) {
-          return done(stats.toString())
-        }
+  it('chunk privacy modules', async () => {
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        webpack(
+          {
+            entry: {
+              scripts: path.join(__dirname, '..', '__fixtures__', 'entry-point.js')
+            },
+            output: {
+              filename: '[name].js',
+              path: path.join(__dirname, '..', '__fixtures__', '/tmp')
+            },
+            plugins: [new PageKitCodeSplittingPlugin()]
+          },
+          function (err, stats) {
+            if (err) {
+              throw err
+            } else if (stats.hasErrors()) {
+              throw stats.toString()
+            }
 
-        const files = stats.toJson().assets.map((x) => x.name)
+            const files = stats.toJson().assets.map((x) => x.name)
 
-        expect(files).toEqual(expect.arrayContaining(['privacy-components.js']));
+            expect(files).toEqual(expect.arrayContaining(['privacy-components.js']))
 
-        done()
-      }
+            resolve()
+          }
+        )
+      }, 0)
     )
   })
 })
