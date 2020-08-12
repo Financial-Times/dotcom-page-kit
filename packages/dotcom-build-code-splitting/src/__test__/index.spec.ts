f6c6f383adc5bf082d0c5293fa714de2ae7d3bb6
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2020-08-12 10:24:43 +0100

    chore(code-splitting): refactor folder structure

---
+++ packages/dotcom-build-code-splitting/src/__test__/index.spec.ts
@@ -0,0 +1,36 @@
+import { PageKitCodeSplittingPlugin } from '../index'
+import webpack from 'webpack'
+import path from 'path'
+
+describe('Code Splitting', () => {
+  it('chunk privacy modules', async () => {
+    await new Promise((resolve) =>
+      webpack(
+        {
+          mode: 'none',
+          entry: {
+            scripts: path.join(__dirname, '/__fixtures__', 'entry-point.js')
+          },
+          output: {
+            filename: '[name].js',
+            path: path.join(__dirname, '/tmp')
+          },
+          plugins: [new PageKitCodeSplittingPlugin()]
+        },
+        function (err, stats) {
+          if (err) {
+            throw err
+          } else if (stats.hasErrors()) {
+            throw stats.toString()
+          }
+
+          const files = stats.toJson().assets.map((x) => x.name)
+
+          expect(files).toEqual(expect.arrayContaining(['privacy-components.js']))
+
+          resolve()
+        }
+      )
+    )
+  })
+})
