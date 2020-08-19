042e92ff8fc1267d78ee4a1003aabc83bff8457c
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2020-08-19 17:07:11 +0100

    chore(build-images): convert arguments to options object

--- packages/dotcom-build-images/src/index.ts
+++ packages/dotcom-build-images/src/index.ts
@@ -3,16 +3,19 @@ const glob = require('glob')
 const MultiEntryPlugin = require('webpack/lib/MultiEntryPlugin')
 
 export class PageKitImagesPlugin {
-  basePath: string
+  options: { basePath: string }
 
-  constructor(directory = './client') {
-    this.basePath = directory
+  constructor(options = {}) {
+    this.options = {
+      basePath: './client',
+      ...options
+    }
   }
 
   apply(compiler) {
     const imageFiles = glob.sync('**/*.{png,jpg,jpeg,gif,webp,ico,svg}', {
       absolute: true,
-      cwd: path.resolve(this.basePath)
+      cwd: path.resolve(this.options.basePath)
     })
 
     new MultiEntryPlugin(compiler.context, imageFiles, '__images__').apply(compiler)
@@ -28,9 +31,9 @@ export class PageKitImagesPlugin {
           options: {
             name: (resourcePath) => {
               const dirname = path.dirname(resourcePath)
+              const relativePath = path.relative(this.options.basePath, dirname)
 
-              const relativePath = path.relative(this.basePath, dirname)
-
+              // retain relative directory structure
               return path.join(relativePath, outputFileName)
             }
           }
