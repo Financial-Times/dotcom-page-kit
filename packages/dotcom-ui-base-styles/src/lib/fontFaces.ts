6ec0113d84701f05595c8486003d0c0fe6add3ee
notlee <lee.moody@ft.com>
2021-04-06 14:08:27 +0100

    Require the latest o-typography/o-fonts release
    `dotcom-page-kit` preloads fonts from `o-fonts-assets` however
    the version to use is hard-coded and not tied to the actual
    version of `o-fonts` being installed. This update makes sure the
    fonts which are preloaded and used are aligned, as a new version
    of `o-fonts-assets` has been released.
    
    Ideally we'd add a JS API to Origami, in addition to the o-font
    Sass variable, so `dotcom-page-kit` can grab the correct font
    urls to preload. This might tie in to some future work the
    core team are considering to define variables in a JS/JSON format
    which compile to Sass (a.k.a design tokens) so for now let's keep
    this in sync manually.
    
    Similar past PR:
    https://github.com/Financial-Times/dotcom-page-kit/pull/826

--- packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
+++ packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
@@ -1,8 +1,8 @@
 // NOTE: please also update ./styles.css when updating the URLs defined below. They
 // need to stay in-sync to ensure our CSS output matches the resource hints we set.
 export const fontFaceURLs = [
-  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.5.0/MetricWeb-Regular.woff2',
-  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.5.0/MetricWeb-Semibold.woff2',
-  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.5.0/FinancierDisplayWeb-Regular.woff2',
-  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.5.0/FinancierDisplayWeb-Bold.woff2'
+  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.7.0/MetricWeb-Regular.woff2',
+  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.7.0/MetricWeb-Semibold.woff2',
+  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.7.0/FinancierDisplayWeb-Regular.woff2',
+  'https://www.ft.com/__origami/service/build/v2/files/o-fonts-assets@1.7.0/FinancierDisplayWeb-Bold.woff2'
 ]
