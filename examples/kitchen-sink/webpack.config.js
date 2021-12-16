41e5a6726b2712a0214cbf39e488be143c42ed91
Estefania Morton <estefania.morton@ft.com>
2021-12-16 10:52:05 -0600

    remove dotcom-build-bower-resolve

--- examples/kitchen-sink/webpack.config.js
+++ examples/kitchen-sink/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const { PageKitBasePlugin } = require('@financial-times/dotcom-build-base')
 const { PageKitCodeSplittingPlugin } = require('@financial-times/dotcom-build-code-splitting')
-const { PageKitBowerResolvePlugin } = require('@financial-times/dotcom-build-bower-resolve')
 const { PageKitSassPlugin } = require('@financial-times/dotcom-build-sass')
 const { PageKitJsPlugin } = require('@financial-times/dotcom-build-js')
 
@@ -16,16 +15,12 @@ module.exports = {
     new PageKitBasePlugin(),
     new PageKitJsPlugin(),
     new PageKitCodeSplittingPlugin(),
-    new PageKitBowerResolvePlugin(),
     new PageKitSassPlugin({
       // Enabling webpackImporter because Sass itself can only resolve partial files based on the
       // CWD and not relative to the current file being processed. This means Sass can't find the
       // nested dependencies created when symlinking.
       webpackImporter: true,
-      includePaths: [
-        // All Bower components are installed at the repo root
-        path.resolve('../../bower_components')
-      ]
+      includePaths: [path.resolve('../../node_modules')]
     })
-  ],
+  ]
 }
