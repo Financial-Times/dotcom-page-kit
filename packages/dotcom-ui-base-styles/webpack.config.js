3f45bc05d7bcf475fd645aeff3410254e0d9688b
Serena <serena.chan@ft.com>
2022-04-27 11:41:59 +0100

    added webpack to dotcom-ui-base-styles to transpile o-utils

---
+++ packages/dotcom-ui-base-styles/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.m?js$/,
+        include: '/node_modules/@financial-times/o-utils',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist', 'node'),
+    libraryTarget: 'commonjs'
+  },
+  mode: 'development'
+}
