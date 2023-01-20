b43a43a45e085d7d6f63f833c91431b593f7c5ee
Ivo Murrell <ivo.murrell@ft.com>
2023-01-20 18:20:26 +0000

    test(ft-ui): update selectors to new navigation href

--- examples/ft-ui/__test__/subscribed-user.test.js
+++ examples/ft-ui/__test__/subscribed-user.test.js
@@ -7,7 +7,7 @@ describe('examples/ft-ui', () => {
   describe('Header link elements', () => {
     it('renders the expected loggin-in user Header link elements', async () => {
       await expect(page).toMatchElement('.o-header__top-column--right a[href="/myft"]', { text: 'myFT' })
-      await expect(page).toMatchElement('.o-header__nav-list--right a[href="https://www.ft.com/myaccount"]', {
+      await expect(page).toMatchElement('.o-header__nav-list--right a[href$="/myaccount"]', {
         text: 'Settings & Account'
       })
 
