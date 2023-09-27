112e2f6cf90c06f00cc9930a0f61cbc661710c64
Kara Brightwell <kara@153.io>
2023-09-27 13:14:11 +0100

    build: update jest and all associated packages

--- packages/dotcom-build-images/src/index.ts
+++ packages/dotcom-build-images/src/index.ts
@@ -18,18 +18,24 @@ export class PageKitImagesPlugin {
       cwd: path.resolve(this.options.basePath)
     })
 
-    for(const imageFile of imageFiles) {
+    for (const imageFile of imageFiles) {
       new EntryPlugin(compiler.context, imageFile, '__images__').apply(compiler)
     }
 
     const isDevMode = compiler.options.mode === 'development'
-    const outputFileName = isDevMode ? '[name].[ext]' : '[name].[contenthash:12].[ext]'
+    const outputFileName = isDevMode ? '[name][ext]' : '[name].[contenthash:12][ext]'
 
     compiler.options.module.rules.push({
       test: /\.(png|jpe?g|gif|webp|ico|svg)$/,
       type: 'asset/resource',
       generator: {
-        filename: outputFileName
+        filename: (pathInfo) => {
+          const dirname = path.dirname(pathInfo.module.resource)
+          const relativePath = path.relative(this.options.basePath, dirname)
+
+          // retain relative directory structure
+          return path.join(relativePath, outputFileName)
+        }
       }
     })
   }
