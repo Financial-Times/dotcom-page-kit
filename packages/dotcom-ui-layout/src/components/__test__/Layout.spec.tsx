878f5fd819b496505e18f58b71f24d814f77068e
Ben Wallman <benwallman@hotmail.com>
2023-09-29 16:46:26 +0100

    Committing even more tests. The stubbing implementation approach is far more greppable and scalable, really in love with it :)

--- packages/dotcom-ui-layout/src/components/__test__/Layout.spec.tsx
+++ packages/dotcom-ui-layout/src/components/__test__/Layout.spec.tsx
@@ -1,226 +1,306 @@
 /**
  * @jest-environment jsdom
  */
-import 'jest-enzyme'
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render } from '@testing-library/react'
 
 import { Layout as Subject } from '../Layout'
-import { Header, Drawer, LogoOnly, NoOutboundLinksHeader } from '@financial-times/dotcom-ui-header'
-import { Footer, LegalFooter } from '@financial-times/dotcom-ui-footer'
+
+jest.mock('@financial-times/dotcom-ui-header', () => ({
+  Header: () => <div>Header mock</div>,
+  Drawer: () => <div>Drawer mock</div>,
+  LogoOnly: () => <div>LogoOnly mock</div>,
+  NoOutboundLinksHeader: () => <div>NoOutboundLinksHeader mock</div>,
+}))
+
+jest.mock('@financial-times/dotcom-ui-footer', () => ({
+  Footer: () => <div>Footer mock</div>,
+  LegalFooter: () => <div>LegalFooter mock</div>,
+}))
 
 describe('dotcom-ui-layout/src/components/Layout', () => {
-  const result = shallow(<Subject />)
-
   it('renders skip links to page landmarks', () => {
-    expect(result.find('a[href="#site-navigation"]')).toHaveText('Skip to navigation')
-    expect(result.find('a[href="#site-content"]')).toHaveText('Skip to content')
-    expect(result.find('a[href="#site-footer"]')).toHaveText('Skip to footer')
+    const { container } = render(
+      <Subject />
+    )
+
+    const skipToNavigationLink = container.querySelector('a[href="#site-navigation"]')
+    const skipToContentLink = container.querySelector('a[href="#site-content"]')
+    const skipToFooterLink = container.querySelector('a[href="#site-footer"]')
+
+    expect(skipToNavigationLink?.innerHTML).toContain('Skip to navigation')
+    expect(skipToContentLink?.innerHTML).toContain('Skip to content')
+    expect(skipToFooterLink?.innerHTML).toContain('Skip to footer')
   })
 
-  it('renders the header and drawer components', () => {
-    expect(result.find(Header)).toExist()
-    expect(result.find(Drawer)).toExist()
-  })
+  it('renders the header, drawer & footer components', () => {
+    const { container } = render(
+      <Subject />
+    )
 
-  it('renders the footer component', () => {
-    expect(result.find(Footer)).toExist()
+    expect(container.innerHTML).toContain('Header mock')
+    expect(container.innerHTML).toContain('Drawer mock')
+    expect(container.innerHTML).toContain('Footer mock')
   })
 
   describe('header variations', () => {
     describe('with the simple variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject headerVariant={'simple' as any} />)
-      })
-
       it('renders the header component', () => {
-        expect(result.find(Header)).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'simple' as any}
+          />
+        )
+
+        expect(container.innerHTML).toContain('Header mock')
       })
 
       it('renders the drawer component', () => {
-        expect(result.find(Drawer)).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'simple' as any}
+          />
+        )
+
+        expect(container.innerHTML).toContain('Drawer mock')
       })
 
       it('renders the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'simple' as any}
+          />
+        )
+
+        expect(
+          container.querySelector('a[href="#site-navigation"]')
+        ).not.toBeNull()
       })
     })
 
     describe('with the large-logo variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject headerVariant={'large-logo' as any} />)
-      })
-
       it('renders the header component', () => {
-        expect(result.find(Header)).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'large-logo' as any}
+          />
+        )
+
+        expect(container.innerHTML).toContain('Header mock')
       })
 
       it('renders the drawer component', () => {
-        expect(result.find(Drawer)).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'large-logo' as any}
+          />
+        )
+
+        expect(container.innerHTML).toContain('Drawer mock')
       })
 
       it('renders the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).toExist()
+        const { container } = render(
+          <Subject
+            headerVariant={'large-logo' as any}
+          />
+        )
+
+        expect(container.querySelector('a[href="#site-navigation"]')).not.toBeNull()
       })
     })
 
     describe('with the logo-only variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject headerVariant={'logo-only' as any} />)
-      })
-
       it('renders the logo only header component', () => {
-        expect(result.find(LogoOnly)).toExist()
+        const { container } = render(
+          <Subject headerVariant={'logo-only' as any} />
+        )
+
+        expect(container.innerHTML).toContain('LogoOnly mock')
       })
 
       it('does not render the drawer component', () => {
-        expect(result.find(Drawer)).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={'logo-only' as any} />
+        )
+
+        expect(container.innerHTML).not.toContain('Drawer mock')
       })
 
       it('does not render the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={'logo-only' as any} />
+        )
+
+        expect(container.querySelector('a[href="#site-navigation"]')).toBeNull()
       })
     })
 
     describe('with the no-outbound-links variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject headerVariant={'no-outbound-links' as any} />)
-      })
-
       it('renders the no-outbound-links header component', () => {
-        expect(result.find(NoOutboundLinksHeader)).toExist()
+        const { container } = render(
+          <Subject headerVariant={'no-outbound-links' as any} />
+        )
+
+        expect(container.innerHTML).toContain('NoOutboundLinksHeader mock')
       })
 
       it('does not render the drawer component', () => {
-        expect(result.find(Drawer)).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={'no-outbound-links' as any} />
+        )
+
+        expect(container.innerHTML).not.toContain('Drawer mock')
       })
 
       it('does not render the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={'no-outbound-links' as any} />
+        )
+
+        expect(container.querySelector('a[href="#site-navigation"]')).toBeNull()
       })
     })
 
     describe('with the header disabled', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject headerVariant={false} />)
-      })
-
       it('does not render the header component', () => {
-        expect(result.find(Header)).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={false} />
+        )
+
+        expect(container.innerHTML).not.toContain('Header mock')
       })
 
       it('does not render the drawer component', () => {
-        expect(result.find(Drawer)).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={false} />
+        )
+
+        expect(container.innerHTML).not.toContain('Drawer mock')
       })
 
       it('does not render the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).not.toExist()
+        const { container } = render(
+          <Subject headerVariant={false} />
+        )
+
+        expect(container.querySelector('a[href="#site-navigation"]')).toBeNull()
       })
     })
 
     describe('with a custom header component', () => {
-      let result
-
-      beforeAll(() => {
-        const customHeader = <header>My custom header</header>
-        result = shallow(<Subject headerComponent={customHeader} />)
-      })
-
       it('renders the given component', () => {
-        expect(result.find('header')).toHaveText('My custom header')
+        const { container } = render(
+          <Subject headerComponent={<header>My custom header</header>} />
+        )
+
+        expect(container.innerHTML).toContain('My custom header')
       })
 
       it('does not render the header component', () => {
-        expect(result.find(Header)).not.toExist()
+        const { container } = render(
+          <Subject headerComponent={<header>My custom header</header>} />
+        )
+
+        expect(container.innerHTML).not.toContain('Header mock')
       })
 
       it('does not render the drawer component', () => {
-        expect(result.find(Drawer)).not.toExist()
+        const { container } = render(
+          <Subject headerComponent={<header>My custom header</header>} />
+        )
+
+        expect(container.innerHTML).not.toContain('Drawer mock')
       })
 
       it('does not render the navigation skip link', () => {
-        expect(result.find('a[href="#site-navigation"]')).not.toExist()
+        const { container } = render(
+          <Subject headerComponent={<header>My custom header</header>} />
+        )
+
+        expect(container.querySelector('a[href="#site-navigation"]')).toBeNull()
       })
     })
   })
 
   describe('footer variations', () => {
     describe('with the simple variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject footerVariant={'simple' as any} />)
-      })
-
       it('renders the footer component', () => {
-        expect(result.find(Footer)).toExist()
+        const { container } = render(
+          <Subject footerVariant={'simple' as any} />
+        )
+
+        expect(container.innerHTML).toContain('Footer mock')
       })
 
       it('renders the footer skip link', () => {
-        expect(result.find('a[href="#site-footer"]')).toExist()
+        const { container } = render(
+          <Subject footerVariant={'simple' as any} />
+        )
+
+        expect(container.querySelector('a[href="#site-footer"]')).not.toBeNull()
       })
     })
 
     describe('with the legal variant', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject footerVariant={'legal' as any} />)
-      })
-
       it('renders the legal footer component', () => {
-        expect(result.find(LegalFooter)).toExist()
+        const { container } = render(
+          <Subject footerVariant={'legal' as any} />
+        )
+
+        expect(container.innerHTML).toContain('LegalFooter mock')
       })
 
       it('renders the footer skip link', () => {
-        expect(result.find('a[href="#site-footer"]')).toExist()
+        const { container } = render(
+          <Subject footerVariant={'legal' as any} />
+        )
+
+        expect(container.querySelector('a[href="#site-footer"]')).not.toBeNull()
       })
     })
 
     describe('with the footer disabled', () => {
-      let result
-
-      beforeAll(() => {
-        result = shallow(<Subject footerVariant={false} />)
-      })
-
       it('does not render the footer component', () => {
-        expect(result.find(Footer)).not.toExist()
+        const { container } = render(
+          <Subject footerVariant={false} />
+        )
+
+        expect(container.innerHTML).not.toContain('Footer mock')
       })
 
       it('does not render the footer skip link', () => {
-        expect(result.find('a[href="#site-footer"]')).not.toExist()
+        const { container } = render(
+          <Subject footerVariant={false} />
+        )
+
+        expect(container.querySelector('a[href="#site-footer"]')).toBeNull()
       })
     })
 
     describe('with a custom footer component', () => {
-      let result
-
-      beforeAll(() => {
-        const customFooter = <footer>My custom footer</footer>
-        result = shallow(<Subject footerComponent={customFooter} />)
-      })
-
       it('renders the given component', () => {
-        expect(result.find('footer')).toHaveText('My custom footer')
+        const { container } = render(
+          <Subject footerComponent={<footer>My custom footer</footer>} />
+        )
+
+        expect(container.innerHTML).toContain('My custom footer')
       })
 
       it('does not render the footer component', () => {
-        expect(result.find(Footer)).not.toExist()
+        const { container } = render(
+          <Subject footerComponent={<footer>My custom footer</footer>} />
+        )
+
+        expect(container.innerHTML).not.toContain('Footer mock')
       })
 
       it('does not render the footer skip link', () => {
-        expect(result.find('a[href="#site-footer"]')).not.toExist()
+        const { container } = render(
+          <Subject footerComponent={<footer>My custom footer</footer>} />
+        )
+
+        expect(container.querySelector('a[href="#site-footer"]')).toBeNull()
       })
     })
   })
