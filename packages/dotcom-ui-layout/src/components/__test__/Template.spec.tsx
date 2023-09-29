6eeabd157378945bee20f026034a308958090178
Ben Wallman <benwallman@hotmail.com>
2023-09-29 17:08:55 +0100

    Updated to get layout & header tests working

--- packages/dotcom-ui-layout/src/components/__test__/Template.spec.tsx
+++ packages/dotcom-ui-layout/src/components/__test__/Template.spec.tsx
@@ -1,24 +1,24 @@
 /**
  * @jest-environment jsdom
  */
-import 'jest-enzyme'
 import React from 'react'
-import { mount } from 'enzyme'
+import { render } from '@testing-library/react'
 
 import Subject from '../Template'
 
 describe('dotcom-ui-layout/src/components/Template', () => {
   it('can render contents provided as a string', () => {
-    const result = mount(<Subject>{'<p>Hello World</p>'}</Subject>)
-    expect(result.html()).toBe('<div><p>Hello World</p></div>')
+    const { container } = render(<Subject>{'<p>Hello World</p>'}</Subject>)
+
+    expect(container.outerHTML).toContain('<div><p>Hello World</p></div>')
   })
 
   it('can render contents provided as children', () => {
-    const result = mount(
+    const { container } = render(
       <Subject>
         <p>Hello World</p>
       </Subject>
     )
-    expect(result.html()).toBe('<div><p>Hello World</p></div>')
+    expect(container.outerHTML).toContain('<div><p>Hello World</p></div>')
   })
 })
