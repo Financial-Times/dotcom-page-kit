6e7bd523677226647ef427b6c739f223dddf396e
Andrew Georgiou <andrewgeorgiou@Andrews-Mini.cable.virginm.net>
2023-10-11 09:26:59 +0100

    Preparation for CMP integration, importing a util function from the Privacy package which will update the 'manage cookies' link to integrate with the CMP modal

--- packages/dotcom-ui-footer/browser.js
+++ packages/dotcom-ui-footer/browser.js
@@ -1,4 +1,9 @@
 import Footer from '@financial-times/o-footer'
+import * as flags from '@financial-times/dotcom-ui-flags'
+import { updateFooterLinkCMP } from '@financial-times/privacy-utils'
+
+const flagsClient = flags.init()
+
 
 /**
  * @typedef FooterOptions
@@ -11,6 +16,10 @@ import Footer from '@financial-times/o-footer'
  */
 export const init = (footerOptions = {}) => {
   Footer.init(footerOptions.rootElement)
+
+  if (flagsClient.get('adsDisableInternalCMP')) {
+    updateFooterLinkCMP();
+   }
 }
 
 export { Footer as OrigamiFooter }
