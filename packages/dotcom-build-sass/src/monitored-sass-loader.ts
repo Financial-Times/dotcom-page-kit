024d4cc296a9796a26665ea6419f50637538a0ba
Lee Moody <lee.moody@ft.com>
2024-01-23 18:13:57 +0000

    Update local sass build time monitoring
    Set a notification strategy to avoid spam and allow configuration

--- packages/dotcom-build-sass/src/monitored-sass-loader.ts
+++ packages/dotcom-build-sass/src/monitored-sass-loader.ts
@@ -4,8 +4,19 @@ import os from 'os'
 import sassLoader from 'sass-loader'
 
 class SassStats {
-  #noticeThrottle = 10 * 1000
-  #stats = { totalTime: 0, notice: null }
+  #noticeStrategies = ['throttle', 'never', 'always']
+  #noticeStrategy = this.#noticeStrategies.includes(process.env.FT_SASS_STATS_NOTICE)
+    ? process.env.FT_SASS_STATS_NOTICE
+    : 'throttle'
+  #noticeThrottleSeconds =
+    typeof process.env.FT_SASS_STATS_NOTICE_THROTTLE_SECONDS === 'number'
+      ? process.env.FT_SASS_STATS_NOTICE_THROTTLE_SECONDS
+      : 60 * 60 * 0.5 // show throttled notice given 30 mins since last notice
+  #noticeThrottlePercentage =
+    typeof process.env.FT_SASS_STATS_NOTICE_THROTTLE_PERCENTAGE === 'number'
+      ? process.env.FT_SASS_STATS_NOTICE_THROTTLE_PERCENTAGE
+      : 30 // show throttled notice given a 30% increase
+  #stats = { totalTime: 0, noticeDate: null, totalTimeAtLastNotice: 0 }
   #directory = path.join(os.tmpdir(), 'dotcom-build-sass')
   #file = path.join(this.#directory, 'sass-stats.json')
   #startTime
@@ -40,24 +51,60 @@ class SassStats {
     fs.writeFileSync(this.#file, JSON.stringify(this.#stats))
   }
 
-  throttledReport = () => {
-    if (!this.#stats.notice || this.#stats.notice < Date.now() - this.#noticeThrottle) {
-      this.#write({ notice: Date.now() })
+  reportAccordingToNoticeStrategy = () => {
+    let shouldReport
+
+    switch (this.#noticeStrategy) {
+      case 'never':
+        shouldReport = false
+        break
+
+      case 'always':
+        shouldReport = true
+        break
+
+      case 'throttle':
+        // Throttle notices to show a limited number per hour, or if the total sass build time
+        // has increased by a significant percentage. This favours more frequent reports to begin with.
+        const noticeTimeThrottle = Date.now() >= this.#stats.noticeDate + this.#noticeThrottleSeconds * 1000
+        const percentageTotalTimeThrottle =
+          this.#stats.totalTime > 0 &&
+          (this.#stats.totalTime / this.#stats.totalTimeAtLastNotice - 1) * 100 >=
+            this.#noticeThrottlePercentage // % increase
+        shouldReport = !this.#stats.noticeDate || noticeTimeThrottle || percentageTotalTimeThrottle
+        break
+
+      default:
+        break
+    }
+
+    if (shouldReport) {
       this.#report()
     }
   }
 
   #report = () => {
-    const seconds = Math.floor(this.#stats.totalTime / 1000)
-    const minutes = Math.floor(seconds / 60)
-    const hours = Math.floor(seconds / 3600)
-    const time = hours > 2 ? `${hours} hours` : minutes > 2 ? `${minutes} minutes` : `${seconds} seconds`
+    const seconds = this.#stats.totalTime / 1000
+    const minutes = seconds / 60
+    const hours = seconds / 3600
+    const time =
+      hours > 1
+        ? `${hours.toFixed(1)} hours`
+        : minutes > 1
+        ? `${minutes.toFixed(0)} minutes`
+        : `${seconds.toFixed(0)} seconds`
+    const emoji =
+      hours > 2 ? ['🔥', '😭', '😱'] : hours >= 1 ? ['🔥', '😱'] : minutes > 10 ? ['⏱️', '😬'] : ['⏱️']
 
     // eslint-disable-next-line no-console
     console.log(
-      `\n\nYou have spent at least 🔥😱 ${time} 😱🔥 waiting on FT Sass to compile.` +
-        `\nLet's fix that! 🎉 [link to support to optimise / remove Sass]\n\n`
+      `\n\nYou have spent at least ${emoji.join(' ')} ${time} ${emoji
+        .reverse()
+        .join(' ')} waiting on FT Sass to compile.` +
+        `\nLet's fix that! 🎉 https://origami.ft.com/blog/2024/01/24/sass-build-times/\n\n`
     )
+
+    this.#write({ noticeDate: Date.now(), totalTimeAtLastNotice: this.#stats.totalTime })
   }
 }
 
@@ -71,7 +118,10 @@ const forgivingProxy = (target, task) => {
       } catch (error) {
         Reflect.apply(...args)
         // eslint-disable-next-line no-console
-        console.log('dotcom-build-sass: Failed to monitor Sass build.', error)
+        console.log(
+          'dotcom-build-sass: Failed to monitor Sass build. Please report to #origami-support',
+          error
+        )
       }
     }
   })
@@ -93,7 +143,7 @@ const monitoredSassLoaderProxy = forgivingProxy(sassLoader, (target, sassLoaderT
       // sass-loader's callback has been... called.
       // Either we have sass, or the build failed.
       localStats.end()
-      localStats.throttledReport()
+      localStats.reportAccordingToNoticeStrategy()
       return Reflect.apply(target, thisArg, argumentsList)
     })
   })
