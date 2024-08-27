ec8b47572c17beef08ae760f136d275f2b684b9f
metart43 <artem.metelskyi@ft.com>
2024-08-27 14:50:52 +0100

    feat: (dotcom-ui-header) integrate new Typeahead
    Adds the new Typeahead to the search container. A new container with `role="listbox"` is created, semantically linking the search input to the suggestions container. The Typeahead is now built with Preact, so initialization has been updated.

--- packages/dotcom-ui-header/browser.js
+++ packages/dotcom-ui-header/browser.js
@@ -1,5 +1,6 @@
 import Header from '@financial-times/o-header'
-import TopicSearch from 'n-topic-search'
+import TypeAhead from 'n-topic-search'
+import { h, render } from 'preact'
 
 /**
  * @typedef HeaderOptions
@@ -16,7 +17,29 @@ export const init = (headerOptions = {}) => {
     '.o-header [data-n-topic-search], .o-header__drawer [data-n-topic-search]'
   )
   topicSearchElements.forEach((element) => {
-    new TopicSearch(element, headerOptions)
+    const oheaderContainer = element.parentElement
+    const oheaderContainerParent = oheaderContainer?.parentElement
+    const isDrawer = oheaderContainerParent?.parentElement?.getAttribute('data-o-header-drawer') === 'true'
+
+    const form = oheaderContainer?.querySelector('form')
+    const input = form?.querySelector('input')
+    const typeaheadContainer = document.createElement('div')
+    typeaheadContainer.id = `suggestions-${input?.id}`
+    typeaheadContainer.className = 'typeahead__main-container'
+    typeaheadContainer.role = 'listbox'
+
+    if (!form || !input) return
+
+    if (isDrawer) {
+      const mobileDrawer = oheaderContainerParent?.parentElement
+      element.appendChild(typeaheadContainer)
+
+      render(<TypeAhead container={mobileDrawer} inputId={input.id} />, typeaheadContainer)
+    } else {
+      form.after(typeaheadContainer)
+
+      render(<TypeAhead container={oheaderContainerParent} inputId={input.id} />, typeaheadContainer)
+    }
   })
 
   Header.init(headerOptions.rootElement)
