7a7afbe3161792b254706b3a46454bf0142c12f2
metart43 <artem.metelskyi@ft.com>
2024-08-27 09:57:21 +0100

    feat: (dotcom-ui-header) bring new `o-header` mark up for the sticky and regular searches

--- packages/dotcom-ui-header/src/components/search/partials.tsx
+++ packages/dotcom-ui-header/src/components/search/partials.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Search = ({ instance }) => {
+  const inputId = `o-header-search-term-${instance}`
   return (
     <div
       id={`o-header-search-${instance}`}
       className={`o-header__row o-header__search o-header__search--${instance}`}
       data-trackable="header-search"
-      data-o-header-search>
+      data-o-header-search
+    >
       <div className="o-header__container">
         <form
           className="o-header__search-form"
@@ -14,11 +16,8 @@ const Search = ({ instance }) => {
           role="search"
           aria-label="Site search"
           data-n-topic-search
-          data-n-topic-search-categories="concepts,equities"
-          data-n-topic-search-view-all>
-          <label
-            htmlFor={`o-header-search-term-${instance}`}
-            className="o-header__search-term o-forms-field o-forms-field--optional">
+        >
+          <label htmlFor={inputId} className="o-header__search-term o-forms-field o-forms-field--optional">
             <span className="o-forms-title o-header__visually-hidden">
               <span className="o-forms-title__main">
                 Search the <abbr title="Financial Times">FT</abbr>
@@ -26,28 +25,30 @@ const Search = ({ instance }) => {
             </span>
             <span className="o-forms-input o-forms-input--text o-forms-input--suffix">
               <input
-                className="o-header__search-term"
-                id={`o-header-search-term-${instance}`}
+                id={inputId}
                 name="q"
-                type="text"
+                type="search"
                 autoComplete="off"
                 autoCorrect="off"
                 autoCapitalize="off"
                 spellCheck={false}
-                data-trackable="search-term"
-                data-n-topic-search-input
                 placeholder="Search for stories, topics or securities"
+                role="combobox"
+                aria-controls={`suggestions-${inputId}`}
               />
               <button className="o-header__search-submit" type="submit">
-                Search
+                <span aria-hidden="true" className="o-header__search-icon"></span>
+                <span>Search</span>
               </button>
               <button
                 className="o-header__search-close o--if-js"
                 type="button"
                 aria-controls={`o-header-search-${instance}`}
                 title="Close search bar"
-                data-trackable="close">
+                data-trackable="close"
+              >
                 <span className="o-header__visually-hidden">Close search bar</span>
+                <span>Close</span>
               </button>
             </span>
           </label>
