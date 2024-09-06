322151d1f469663d50a48fece0cf5467d04cce83
daniel <>
2024-09-06 09:58:07 +0200

    add visually-hidden to close icon text

--- packages/dotcom-ui-header/src/components/search/partials.tsx
+++ packages/dotcom-ui-header/src/components/search/partials.tsx
@@ -48,7 +48,7 @@ const Search = ({ instance }) => {
                 data-trackable="close"
               >
                 <span className="o-header__visually-hidden">Close search bar</span>
-                <span>Close</span>
+                <span className="o-header__visually-hidden">Close</span>
               </button>
             </span>
           </label>
