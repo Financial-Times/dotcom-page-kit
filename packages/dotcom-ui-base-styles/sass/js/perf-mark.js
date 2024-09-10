7214949c630458187a2d9987b6723e607d946090
Kara Brightwell <kara@153.io>
2024-09-10 11:56:35 +0100

    feat!: remove unused javascript utils

--- packages/dotcom-ui-base-styles/sass/js/perf-mark.js
+++
@@ -1,6 +0,0 @@
-module.exports = name => {
-	const performance = window.LUX || window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
-	if (performance && performance.mark) {
-		performance.mark(name);
-	}
-};
