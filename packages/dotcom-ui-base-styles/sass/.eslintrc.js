24042ebbd486e428aeb2db7642e4d6f71ad41647
Kara Brightwell <kara@153.io>
2024-09-16 15:03:13 +0100

    chore: remove all non-sass files

--- packages/dotcom-ui-base-styles/sass/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	extends: ['@financial-times/eslint-config-next'],
-	rules: {
-		// allow console for the deprecation warnings
-		'no-console': 'off'
-	}
-};
--- packages/dotcom-ui-base-styles/sass/secrets.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	whitelist: [
-		'ab1bcc7b3a6544c99260f7608f8e845e' // from a third-party URL in images/_mixins.scss
-	]
-};
