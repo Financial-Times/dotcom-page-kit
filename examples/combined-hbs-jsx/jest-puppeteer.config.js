773e2d0669d8fc8babb4ba70f19fa1861dc6f32f
Oliver Turner <oliverturner@users.noreply.github.com>
2024-10-24 12:26:50 +0100

    Remove Puppeteer launch config

--- examples/combined-hbs-jsx/jest-puppeteer.config.js
+++ examples/combined-hbs-jsx/jest-puppeteer.config.js
@@ -1,11 +1,13 @@
+// const executablePath = process.env.CI ? '/usr/bin/google-chrome-stable' : undefined
+
 module.exports = {
   server: {
     command: 'npm run start',
     port: process.env.PORT || 3456
-  },
-  launch: {
-    // https://discuss.circleci.com/t/puppeteer-fails-on-circleci/22650
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: "/usr/bin/google-chrome-stable"
   }
+  // launch: {
+  //   // https://discuss.circleci.com/t/puppeteer-fails-on-circleci/22650
+  //   args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  //   executablePath
+  // }
 }
