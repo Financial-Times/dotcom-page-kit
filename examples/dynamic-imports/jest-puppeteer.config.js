f35f29b97d7dc990caad345d5e7d4db1397c6a0a
Oliver Turner <oliverturner@users.noreply.github.com>
2024-10-24 12:15:29 +0100

    Specify Chrome executable path

--- examples/dynamic-imports/jest-puppeteer.config.js
+++ examples/dynamic-imports/jest-puppeteer.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   launch: {
     // https://discuss.circleci.com/t/puppeteer-fails-on-circleci/22650
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: "/usr/bin/google-chrome-stable"
   }
 }
