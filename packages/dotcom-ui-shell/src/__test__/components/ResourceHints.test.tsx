44bff7a260d26a8cbd81b4853da878cc836e3407
Sqyphen <tobias.fieldhouse@gmail.com>
2024-11-05 15:02:41 +0000

    Update all Origami Build Service URLs to use page-kit as their source_code

--- packages/dotcom-ui-shell/src/__test__/components/ResourceHints.test.tsx
+++ packages/dotcom-ui-shell/src/__test__/components/ResourceHints.test.tsx
@@ -8,7 +8,7 @@ describe('dotcom-ui-shell/src/components/ResourceHints', () => {
       'www.example.com/assets/style.css',
       'www.example.com/images/graphic.svg#icon',
       '/assets/public/style.as83hd99.css',
-      '/__origami/service/build/v3/font?font_format=woff2&font_name=FinancierDisplayWeb-Bold&system_code=origami&version=1.12'
+      '/__origami/service/build/v3/font?font_format=woff2&font_name=FinancierDisplayWeb-Bold&system_code=page-kit&version=1.12'
     ]
 
     const tree = renderer.create(<Subject resourceHints={fixture} />).toJSON()
