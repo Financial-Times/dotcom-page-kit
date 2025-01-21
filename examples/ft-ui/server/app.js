473e3d7ebc5d74a75093e4fa59fd729921327f59
Kara Brightwell <kara@153.io>
2025-01-21 17:43:10 +0000

    test: use asset loader in ft-ui so it can load production-hashed dependencies

--- examples/ft-ui/server/app.js
+++ examples/ft-ui/server/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import * as navigation from '@financial-times/dotcom-middleware-navigation'
+import * as assets from '@financial-times/dotcom-middleware-asset-loader'
 import { homeController } from './controllers/home.jsx'
 
 export const app = express()
 
 app.use(navigation.init())
+app.use(assets.init({ hostStaticAssets: true }))
 
 app.use('/public', express.static('./public'))
 
