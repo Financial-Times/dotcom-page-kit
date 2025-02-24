8867738106e9b0951aef05ad16a9103e0362e9af
Kara Brightwell <kara@153.io>
2025-02-24 12:21:44 +0000

    feat: use use-case tokens for document styles with fallback

--- packages/dotcom-ui-base-styles/src/lib/documentStyles.ts
+++ packages/dotcom-ui-base-styles/src/lib/documentStyles.ts
@@ -3,8 +3,6 @@ import { CSSProperties } from 'react'
 export const documentStyles: CSSProperties = {
   // Enable use of 100vw which does not account for the scroll bar
   overflowX: 'hidden',
-  // o-colors "paper"
-  backgroundColor: '#fff1e5',
-  // o-colors "black-80"
-  color: '#33302e'
+  backgroundColor: 'var(--o3-color-use-case-page-background, #fff1e5)',
+  color: 'var(--o3-color-use-case-body-text, #33302e)'
 }
