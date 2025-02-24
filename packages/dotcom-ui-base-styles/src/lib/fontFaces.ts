8385d92c21e2ce4c652f5de7744adb89c192bc84
Kara Brightwell <kara@153.io>
2025-02-24 12:46:54 +0000

    feat!: switch from o-fonts to o3-foundation

--- packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
+++ packages/dotcom-ui-base-styles/src/lib/fontFaces.ts
@@ -1,8 +1,7 @@
-// NOTE: please also update dotcom-ui-base-styles/styles.scss $o-fonts-path when updating the URLs defined below. They
-// need to stay in-sync to ensure our CSS output matches the resource hints we set.
+// NOTE: these URL need to stay in sync with the URLs in o3-foundation to ensure we're preloading the same fonts we're loading.
+// https://github.com/Financial-Times/origami/blob/main/components/o3-foundation/src/css/components/fonts.css
+// https://github.com/Financial-Times/origami/blob/main/components/o3-foundation/src/css/brands/core.css
 export const fontFaceURLs = [
-  'https://www.ft.com/__origami/service/build/v3/font?font_format=woff2&font_name=MetricWeb-Regular&system_code=page-kit&version=1.12',
-  'https://www.ft.com/__origami/service/build/v3/font?font_format=woff2&font_name=MetricWeb-Semibold&system_code=page-kit&version=1.12',
-  'https://www.ft.com/__origami/service/build/v3/font?font_format=woff2&font_name=FinancierDisplayWeb-Regular&system_code=page-kit&version=1.12',
-  'https://www.ft.com/__origami/service/build/v3/font?font_format=woff2&font_name=FinancierDisplayWeb-Bold&system_code=page-kit&version=1.12'
+  'https://www.ft.com/__origami/service/build/v3/font?version=1.13&font_name=Metric2-VF&system_code=origami&font_format=woff2',
+  'https://www.ft.com/__origami/service/build/v3/font?version=1.13&font_name=FinancierDisplay-VF&system_code=origami&font_format=woff2'
 ]
