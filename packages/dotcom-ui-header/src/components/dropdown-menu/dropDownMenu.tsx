d341da65544f5691a51a829567fabf4a46cb3354
alex-ruskov-ft <aleksandar.ruskov@ft.com>
2025-03-13 12:20:54 +0200

    Improving the component

--- packages/dotcom-ui-header/src/components/dropdown-menu/dropDownMenu.tsx
+++ packages/dotcom-ui-header/src/components/dropdown-menu/dropDownMenu.tsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import DROPDOWN_DEFAULT_LIST from './dropDownProfessionalList.json';
 
-export const DropdownMenu: React.FC = () => {
+export interface DropdownMenuProps {
+  listToDisplay?: {
+    id: string,
+    label: string,
+    href: string,
+    icon: string,
+    hasAccess: boolean,
+    isProfessional: boolean,
+    hasBottomLine: boolean
+  }[];
+  headerTitle?: string;
+}
 
-  return <div className="o-header__professional-dropdown">
-    <button aria-haspopup="true" aria-expanded="false" tabIndex={0} className="o-header__professional-dropdown-button">Menu</button>
-    <div className="o-header__professional-dropdown-content">
-      <div className="o-header__professional-dropdown-title-wrapper">
-        <span className="o-header__professional-dropdown-title">FT PROFESSIONAL ACCOUNT</span>
-        <button className='o-header__professional-dropdown-close-icon cross-icon'/>
+export const DropdownMenu = ({
+    listToDisplay = DROPDOWN_DEFAULT_LIST,
+    headerTitle = 'FT PROFESSIONAL ACCOUNT'
+  }: DropdownMenuProps) => {
+  return (
+    <div className="o-header__professional-dropdown">
+      <button className="o-header__professional-dropdown-button">
+        <span className="o-header__professional-dropdown-icon user-icon" />
+        <span className="o-header__professional-dropdown-icon chevron-icon" />
+      </button>
+
+      <div className="o-header__professional-dropdown-content" tabIndex={-1}>
+        <div className="o-header__professional-dropdown-title-wrapper">
+          <span className="o-header__professional-dropdown-title">{headerTitle}</span>
+          <button className="o-header__professional-dropdown-icon cross-icon" />
+        </div>
+
+        <ul className="o-header__professional-dropdown-list">
+          {listToDisplay.map(link => (
+            <li key={link.id} className={`o-header__professional-dropdown-list-item ${link.hasBottomLine && 'o-header__professional-dropdown-list-divider'}`} >
+              <a className="o-header__professional-dropdown-list-item-link" href={link.href}>
+                <span className={`o-header__professional-dropdown-icon ${link.hasAccess ? link.icon + '-icon' : 'lock-icon'}`} />
+                <span>{link.label}</span>
+                {link.isProfessional && <span className="o-header__professional-dropdown-list-pro-label">FT PROFESSIONAL</span>}
+              </a>
+            </li>
+          ))}
+        </ul >
       </div>
-      <ul className="o-header__professional-dropdown-list">
-        {DROPDOWN_DEFAULT_LIST.map(link => (
-          <li key={link.id} className={`o-header__professional-dropdown-list-item ${link.hasBottomLink && 'o-header__professional-dropdown-list-divider'}`} >
-            <a className="o-header__professional-dropdown-list-item-link" href={link.href}>
-              <span className={`o-header__professional-dropdown-list-icon ${link.hasAccess ? link.icon + '-icon' : 'lock-icon'}`} />
-              <span>{link.label}</span>
-              {link.isProfessional && <span className="o-header__professional-dropdown-list-pro-label">FT PROFESSIONAL</span>}
-            </a>
-          </li>
-        ))}
-      </ul >
     </div>
-  </div >
+  )
 }
