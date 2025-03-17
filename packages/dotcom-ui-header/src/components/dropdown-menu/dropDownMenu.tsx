d6b4fc5456358028c2ed3f2b9b9808d76146dacb
Paola Dimova <paola.dimova@ft.com>
2025-03-17 11:20:31 +0200

    enhance a11y

--- packages/dotcom-ui-header/src/components/dropdown-menu/dropDownMenu.tsx
+++ packages/dotcom-ui-header/src/components/dropdown-menu/dropDownMenu.tsx
@@ -15,29 +15,42 @@ export interface DropdownMenuProps {
 }
 
 export const DropdownMenu = ({
-    listToDisplay = DROPDOWN_DEFAULT_LIST,
-    headerTitle = 'FT PROFESSIONAL ACCOUNT'
-  }: DropdownMenuProps) => {
+  listToDisplay = DROPDOWN_DEFAULT_LIST,
+  headerTitle = 'FT PROFESSIONAL ACCOUNT'
+}: DropdownMenuProps) => {
   return (
-    <div className="o-header__professional-dropdown">
+    <nav className="o-header__professional-dropdown" aria-describedby="dropdown-title">
       {/* Tab index here is needed for making sure safari and ios browsers dropdown behavior works */}
-      <button tabIndex={0} className="o-header__professional-dropdown-button">
-        <span className="o-header__professional-dropdown-icon user-icon" />
-        <span className="o-header__professional-dropdown-icon chevron-icon" />
+      <button
+        className="o-header__professional-dropdown-button"
+        tabIndex={0}
+        aria-label="Dropdown menu has opened on focus, press Tab to access links."
+        aria-controls="dropdown-options"
+      >
+        <span className="o-header__professional-dropdown-icon user-icon" aria-hidden="true" />
+        <span className="o-header__professional-dropdown-icon chevron-icon" aria-hidden="true" />
       </button>
 
-      <div className="o-header__professional-dropdown-content" tabIndex={-1}>
+      <div
+        className="o-header__professional-dropdown-content"
+        tabIndex={-1}
+        id="dropdown-options"
+      >
         <div className="o-header__professional-dropdown-title-wrapper">
-          <span className="o-header__professional-dropdown-title">{headerTitle}</span>
+          <span className="o-header__professional-dropdown-title" id="dropdown-title">{headerTitle}</span>
           {/* Tab index again needed for safari and ios browsers */}
-          <button tabIndex={0} className="o-header__professional-dropdown-icon cross-icon" />
+          <button
+            className="o-header__professional-dropdown-icon cross-icon"
+            tabIndex={0}
+            aria-label="Close dropdown menu"
+          />
         </div>
 
         <ul className="o-header__professional-dropdown-list">
           {listToDisplay.map(link => (
             <li key={link.id} className={`o-header__professional-dropdown-list-item ${link.hasBottomLine && 'o-header__professional-dropdown-list-divider'}`} >
               <a className="o-header__professional-dropdown-list-item-link" href={link.href}>
-                <span className={`o-header__professional-dropdown-icon ${link.hasAccess ? link.icon + '-icon' : 'lock-icon'}`} />
+                <span className={`o-header__professional-dropdown-icon ${link.hasAccess ? link.icon + '-icon' : 'lock-icon'}`} aria-hidden="true"  />
                 <span>{link.label}</span>
                 {link.isProfessional && <span className="o-header__professional-dropdown-list-pro-label">FT PROFESSIONAL</span>}
               </a>
@@ -45,6 +58,6 @@ export const DropdownMenu = ({
           ))}
         </ul >
       </div>
-    </div>
+    </nav>
   )
 }
