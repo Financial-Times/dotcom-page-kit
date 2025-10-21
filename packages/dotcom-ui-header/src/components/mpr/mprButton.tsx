6b7c853d48d2a665f69e62591b420cdda044f640
Ivaylo Vladimirov <ivaylo.vladimirov@ft.com>
2025-10-21 12:15:25 +0300

    ENTST-1459 Add MPR button functionality to header component

---
+++ packages/dotcom-ui-header/src/components/mpr/mprButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { LinkButton } from '@financial-times/o3-button'
+import type { MprButtonProps } from '@financial-times/o-header/src/tsx/components/mpr-button'
+
+const MPR_URL = 'https://professional-monetary-policy-radar.ft.com/'
+
+export const MprButton = ({ id, dataTrackable }: MprButtonProps) => (
+  <LinkButton
+    href={MPR_URL}
+    icon="radar"
+    label="MPR"
+    size="small"
+    type="primary"
+    attributes={{
+      id: id,
+      'data-trackable': dataTrackable,
+      className: `o-header__mpr-button`
+    }}
+  />
+)
--- packages/dotcom-ui-header/src/interfaces.d.ts
+++ packages/dotcom-ui-header/src/interfaces.d.ts
@@ -13,6 +13,7 @@ export type THeaderOptions = {
   showAskButton?: boolean
   showProNavigation?: boolean
   showRestartSubscriptionButton?: boolean
+  showMprButton?: boolean
 }
 
 export type THeaderProps = THeaderOptions & {
