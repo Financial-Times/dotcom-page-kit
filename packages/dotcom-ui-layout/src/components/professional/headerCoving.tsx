2e742184620e6e8c45946971fae6166f242bd8e1
Phelipe Maia <phelipe.maia@ft.com>
2025-12-03 10:46:44 +0000

    fix: avoid uppercase on brand placeholder due to accessibility

--- packages/dotcom-ui-layout/src/components/professional/headerCoving.tsx
+++ packages/dotcom-ui-layout/src/components/professional/headerCoving.tsx
@@ -4,7 +4,7 @@ export const HeaderCoving = () => {
   return (
     <div data-o3-brand="professional" className={`n-layout__pro-coving`}>
       <div className={`n-layout__pro-coving-text`}>
-        <span className={`n-layout__pro-coving-brand`}>FT PROFESSIONAL</span>
+        <span className={`n-layout__pro-coving-brand`}>FT Professional</span>
         <span className={`n-layout__pro-coving-organisation`}></span>
       </div>
     </div>
